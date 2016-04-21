d7b8736564e91758700611a9b03702b43ed76b1c
Chad Weimer <chadweimer@gmail.com>
2016-04-21 07:22:19 -0700

    Updated the color scheme
    And implemented Delete

--- gomp.go
+++ gomp.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "gomp/routers"
+	"gomp/routers"
 
-    "github.com/go-macaron/binding"
-    "gopkg.in/macaron.v1"
+	"github.com/go-macaron/binding"
+	"gopkg.in/macaron.v1"
 )
 
 func main() {
-    m := macaron.Classic()
-    m.Use(macaron.Renderer())
-    m.Use(macaron.Static("public"))
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+	m.Use(macaron.Static("public"))
 
-    m.Get("/", routers.CheckInstalled, routers.Home)
-    m.Group("/recipes", func() {
-        m.Get("/", routers.ListRecipes)
-        m.Get("/:id:int", routers.GetRecipe)
-        m.Get("/create", routers.CreateRecipe)
-        m.Post("/create", binding.Bind(routers.RecipeForm{}), routers.CreateRecipePost)
-        m.Get("/edit/:id:int", routers.EditRecipe)
-        m.Post("/edit/:id:int", binding.Bind(routers.RecipeForm{}), routers.EditRecipePost)
-    }, routers.CheckInstalled)
-    //m.Group("/meals", func() {
-    //  m.Get("/", routers.Meal)
-    //  m.Get(/:id:int, routers.Meals)
-    //}, routers.CheckInstalled)
-    m.Get("/install", routers.Install)
+	m.Get("/", routers.CheckInstalled, routers.Home)
+	m.Group("/recipes", func() {
+		m.Get("/", routers.ListRecipes)
+		m.Get("/:id:int", routers.GetRecipe)
+		m.Get("/create", routers.CreateRecipe)
+		m.Post("/create", binding.Bind(routers.RecipeForm{}), routers.CreateRecipePost)
+		m.Get("/edit/:id:int", routers.EditRecipe)
+		m.Post("/edit/:id:int", binding.Bind(routers.RecipeForm{}), routers.EditRecipePost)
+		m.Get("/delete/:id:int", routers.DeleteRecipe)
+	}, routers.CheckInstalled)
+	//m.Group("/meals", func() {
+	//  m.Get("/", routers.Meal)
+	//  m.Get(/:id:int, routers.Meals)
+	//}, routers.CheckInstalled)
+	m.Get("/install", routers.Install)
 
-    m.Run()
+	m.Run()
 }
--- models/recipe.go
+++ models/recipe.go
@@ -4,77 +4,88 @@ import "database/sql"
 
 // Recipe is the primary model class for recipe storage and retrieval
 type Recipe struct {
-    ID          int64
-    Name        string
-    Description string
+	ID          int64
+	Name        string
+	Description string
 }
 
 func GetRecipeByID(id int64) (*Recipe, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-    var name string
-    var description string
-    err = db.QueryRow("SELECT name, description FROM recipes WHERE id = $1", id).Scan(&name, &description)
-    switch {
-    case err == sql.ErrNoRows:
-        return nil, nil
-    case err != nil:
-        return nil, err
-    default:
-        return &Recipe{ID: id, Name: name, Description: description}, nil
-    }
+	var name string
+	var description string
+	err = db.QueryRow("SELECT name, description FROM recipes WHERE id = $1", id).Scan(&name, &description)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, nil
+	case err != nil:
+		return nil, err
+	default:
+		return &Recipe{ID: id, Name: name, Description: description}, nil
+	}
 }
 
 func ListRecipes() ([]*Recipe, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-    var recipes []*Recipe
-    rows, err := db.Query("SELECT id, name, description FROM recipes")
-    if err != nil {
-        return nil, err
-    }
-    for rows.Next() {
-        var id int64
-        var name string
-        var description string
-        rows.Scan(&id, &name, &description)
-        recipes = append(recipes, &Recipe{ID: id, Name: name, Description: description})
-    }
+	var recipes []*Recipe
+	rows, err := db.Query("SELECT id, name, description FROM recipes")
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var id int64
+		var name string
+		var description string
+		rows.Scan(&id, &name, &description)
+		recipes = append(recipes, &Recipe{ID: id, Name: name, Description: description})
+	}
 
-    return recipes, nil
+	return recipes, nil
 }
 
 func CreateRecipe(name string, description string) (*Recipe, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-    result, err := db.Exec("INSERT INTO recipes (name, description) VALUES ($1, $2)", name, description)
-    if err != nil {
-        return nil, err
-    }
-    id, err := result.LastInsertId()
+	result, err := db.Exec("INSERT INTO recipes (name, description) VALUES ($1, $2)", name, description)
+	if err != nil {
+		return nil, err
+	}
+	id, err := result.LastInsertId()
 
-    return &Recipe{ID: id, Name: name, Description: description}, nil
+	return &Recipe{ID: id, Name: name, Description: description}, nil
 }
 
 func UpdateRecipe(r *Recipe) error {
-    db, err := OpenDatabase()
-    if err != nil {
-        return err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-    _, err = db.Exec("UPDATE recipes SET name = $1, description = $2 WHERE id = $3", r.Name, r.Description, r.ID)
-    return err
+	_, err = db.Exec("UPDATE recipes SET name = $1, description = $2 WHERE id = $3", r.Name, r.Description, r.ID)
+	return err
+}
+
+func DeleteRecipe(id int64) error {
+	db, err := OpenDatabase()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DELETE FROM recipes WHERE id = $1", id)
+	return err
 }
