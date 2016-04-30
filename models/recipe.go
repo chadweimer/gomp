103813afee03771d7a9fd1388119f366ae504185
Chad Weimer <chadweimer@gmail.com>
2016-04-29 23:17:21 -0700

    Added some documentation comments

--- models/recipe.go
+++ models/recipe.go
@@ -1,157 +1,159 @@
 package models
 
 import (
-    "strings"
-    "database/sql"
+	"database/sql"
+	"strings"
 )
 
-// Recipe is the primary model class for recipe storage and retrieval
+// RecipeCompact is the model class for recipe retrieval when displaying
+// summary information (e.g., a list of recipes)
 type RecipeCompact struct {
-    ID          int64
-    Name        string
-    Description string
-    Directions  string
+	ID          int64
+	Name        string
+	Description string
+	Directions  string
 }
 
+// Recipe is the primary model class for recipe storage and retrieval
 type Recipe struct {
-    Tags []string
-    RecipeCompact
+	Tags []string
+	RecipeCompact
 }
 
 func GetRecipeByID(id int64) (*Recipe, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-    var name string
-    var description string
-    var directions string
-    err = db.QueryRow("SELECT name, description, directions FROM recipe WHERE id = $1", id).Scan(&name, &description, &directions)
-    if err == sql.ErrNoRows {
-        return nil, nil
-    }
-    if err != nil {
-        return nil, err
-    }
+	var name string
+	var description string
+	var directions string
+	err = db.QueryRow("SELECT name, description, directions FROM recipe WHERE id = $1", id).Scan(&name, &description, &directions)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
 
-    var tags []string
-    rows, err := db.Query("SELECT tag FROM recipe_tags WHERE recipe_id = $1", id)
-    if err != nil {
-        return nil, err
-    }
-    for rows.Next() {
-        var tag string
-        rows.Scan(&tag)
-        tags = append(tags, tag)
-    }
+	var tags []string
+	rows, err := db.Query("SELECT tag FROM recipe_tags WHERE recipe_id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var tag string
+		rows.Scan(&tag)
+		tags = append(tags, tag)
+	}
 
-    r := &Recipe {
-        Tags: tags,
-    }
-    r.ID = id
-    r.Name = name
-    r.Description = description
-    r.Directions = directions
-    return r, nil
+	r := &Recipe{
+		Tags: tags,
+	}
+	r.ID = id
+	r.Name = name
+	r.Description = description
+	r.Directions = directions
+	return r, nil
 }
 
 func ListRecipes() ([]*RecipeCompact, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
 
-    var recipes []*RecipeCompact
-    rows, err := db.Query("SELECT id, name, description, directions FROM recipe")
-    if err != nil {
-        return nil, err
-    }
-    for rows.Next() {
-        var id int64
-        var name string
-        var description string
-        var directions string
-        rows.Scan(&id, &name, &description, &directions)
-        recipes = append(recipes, &RecipeCompact{ID: id, Name: name, Description: description, Directions: directions})
-    }
+	var recipes []*RecipeCompact
+	rows, err := db.Query("SELECT id, name, description, directions FROM recipe")
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var id int64
+		var name string
+		var description string
+		var directions string
+		rows.Scan(&id, &name, &description, &directions)
+		recipes = append(recipes, &RecipeCompact{ID: id, Name: name, Description: description, Directions: directions})
+	}
 
-    return recipes, nil
+	return recipes, nil
 }
 
 func CreateRecipe(name string, description string, directions string, tags []string) (int64, error) {
-    db, err := OpenDatabase()
-    if err != nil {
-        return -1, err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return -1, err
+	}
+	defer db.Close()
 
-    tx, err := db.Begin()
-    if err != nil {
-        return -1, err
-    }
-    result, err := db.Exec("INSERT INTO recipe (name, description, directions) VALUES ($1, $2, $3)", name, description, directions)
-    if err != nil {
-        return -1, err
-    }
-    id, err := result.LastInsertId()
-    if err != nil {
-        return -1, err
-    }
+	tx, err := db.Begin()
+	if err != nil {
+		return -1, err
+	}
+	result, err := db.Exec("INSERT INTO recipe (name, description, directions) VALUES ($1, $2, $3)", name, description, directions)
+	if err != nil {
+		return -1, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
 
-    for _, tag := range tags {
-        err = addTagToRecipe(db, id, tag)
-        if err != nil {
-            return -1, err
-        }
-    }
-    tx.Commit()
+	for _, tag := range tags {
+		err = addTagToRecipe(db, id, tag)
+		if err != nil {
+			return -1, err
+		}
+	}
+	tx.Commit()
 
-    return id, nil
+	return id, nil
 }
 
 func UpdateRecipe(r *Recipe) error {
-    db, err := OpenDatabase()
-    if err != nil {
-        return err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-    tx, err := db.Begin()
-    if err != nil {
-        return err
-    }
-    _, err = db.Exec("UPDATE recipe SET name = $1, description = $2, directions = $3 WHERE id = $4", r.Name, r.Description, r.Directions, r.ID)
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("UPDATE recipe SET name = $1, description = $2, directions = $3 WHERE id = $4", r.Name, r.Description, r.Directions, r.ID)
 
-    _, err = db.Exec("DELETE FROM recipe_tags WHERE recipe_id = $1", r.ID)
-    if err != nil {
-        return err
-    }
-    for _, tag := range r.Tags {
-        err = addTagToRecipe(db, r.ID, tag)
-        if err != nil {
-            return err
-        }
-    }
-    tx.Commit()
+	_, err = db.Exec("DELETE FROM recipe_tags WHERE recipe_id = $1", r.ID)
+	if err != nil {
+		return err
+	}
+	for _, tag := range r.Tags {
+		err = addTagToRecipe(db, r.ID, tag)
+		if err != nil {
+			return err
+		}
+	}
+	tx.Commit()
 
-    return nil
+	return nil
 }
 
 func addTagToRecipe(db *sql.DB, recipeID int64, tag string) error {
-    _, err := db.Exec("INSERT INTO recipe_tags (recipe_id, tag) VALUES ($1, $2)", recipeID, strings.ToLower(tag))
-    return err
+	_, err := db.Exec("INSERT INTO recipe_tags (recipe_id, tag) VALUES ($1, $2)", recipeID, strings.ToLower(tag))
+	return err
 }
 
 func DeleteRecipe(id int64) error {
-    db, err := OpenDatabase()
-    if err != nil {
-        return err
-    }
-    defer db.Close()
+	db, err := OpenDatabase()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-    _, err = db.Exec("DELETE FROM recipe WHERE id = $1", id)
-    return err
+	_, err = db.Exec("DELETE FROM recipe WHERE id = $1", id)
+	return err
 }
--- routers/recipe.go
+++ routers/recipe.go
@@ -1,121 +1,132 @@
 package routers
 
 import (
-    "fmt"
-    "gomp/models"
-    "net/http"
-    "strconv"
+	"fmt"
+	"gomp/models"
+	"net/http"
+	"strconv"
 
-    "gopkg.in/macaron.v1"
+	"gopkg.in/macaron.v1"
 )
 
+// RecipeForm encapsulates user input on the Create and Edit recipe screens
 type RecipeForm struct {
-    Name        string `binding:"Required"`
-    Description string
-    Directions  string
-    Tags        []string
+	Name             string `binding:"Required"`
+	Description      string
+	Directions       string
+	Tags             []string
+	IngredientAmount []string
+	IngredientUnit   []string
+	IngredientName   []string
 }
 
 // GetRecipe handles retrieving and rendering a single recipe
 func GetRecipe(ctx *macaron.Context) {
-    id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    r, err := models.GetRecipeByID(id)
-    switch {
-    case err != nil:
-        InternalServerError(ctx)
-    case r == nil:
-        NotFound(ctx)
-    default:
-        ctx.Data["Recipe"] = r
-        ctx.HTML(http.StatusOK, "recipe/view")
-    }
+	r, err := models.GetRecipeByID(id)
+	switch {
+	case err != nil:
+		InternalServerError(ctx)
+	case r == nil:
+		NotFound(ctx)
+	default:
+		ctx.Data["Recipe"] = r
+		ctx.HTML(http.StatusOK, "recipe/view")
+	}
 }
 
 // ListRecipes handles retrieving and rending a list of available recipes
 func ListRecipes(ctx *macaron.Context) {
-    recipes, err := models.ListRecipes()
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
-    ctx.Data["Recipes"] = recipes
-    ctx.HTML(http.StatusOK, "recipe/list")
+	recipes, err := models.ListRecipes()
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
+	ctx.Data["Recipes"] = recipes
+	ctx.HTML(http.StatusOK, "recipe/list")
 }
 
+// CreateRecipe handles rendering the create recipe screen
 func CreateRecipe(ctx *macaron.Context) {
-    ctx.HTML(http.StatusOK, "recipe/create")
+	ctx.HTML(http.StatusOK, "recipe/create")
 }
 
+// CreateRecipePost handles processing the supplied
+// form input from the create recipe screen
 func CreateRecipePost(ctx *macaron.Context, form RecipeForm) {
-    id, err := models.CreateRecipe(form.Name, form.Description, form.Directions, form.Tags)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
-    ctx.Redirect(fmt.Sprintf("/recipes/%d", id))
+	id, err := models.CreateRecipe(form.Name, form.Description, form.Directions, form.Tags)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
+	ctx.Redirect(fmt.Sprintf("/recipes/%d", id))
 }
 
+// EditRecipe handles rendering the edit recipe screen
 func EditRecipe(ctx *macaron.Context) {
-    id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    r, err := models.GetRecipeByID(id)
-    switch {
-    case err != nil:
-        InternalServerError(ctx)
-    case r == nil:
-        NotFound(ctx)
-    default:
-        ctx.Data["Recipe"] = r
-        ctx.HTML(http.StatusOK, "recipe/edit")
-    }
+	r, err := models.GetRecipeByID(id)
+	switch {
+	case err != nil:
+		InternalServerError(ctx)
+	case r == nil:
+		NotFound(ctx)
+	default:
+		ctx.Data["Recipe"] = r
+		ctx.HTML(http.StatusOK, "recipe/edit")
+	}
 }
 
+// EditRecipePost handles processing the supplied
+// form input from the edit recipe screen
 func EditRecipePost(ctx *macaron.Context, form RecipeForm) {
-    id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    r, err := models.GetRecipeByID(id)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	r, err := models.GetRecipeByID(id)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    r.Name = form.Name
-    r.Description = form.Description
-    r.Directions = form.Directions
-    r.Tags = form.Tags
-    err = models.UpdateRecipe(r)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
-    ctx.Redirect(fmt.Sprintf("/recipes/%d", id))
+	r.Name = form.Name
+	r.Description = form.Description
+	r.Directions = form.Directions
+	r.Tags = form.Tags
+	err = models.UpdateRecipe(r)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
+	ctx.Redirect(fmt.Sprintf("/recipes/%d", id))
 }
 
+// DeleteRecipe handles deleting the recipe with the given id
 func DeleteRecipe(ctx *macaron.Context) {
-    id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	id, err := strconv.ParseInt(ctx.Params("id"), 10, 64)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    err = models.DeleteRecipe(id)
-    if err != nil {
-        InternalServerError(ctx)
-        return
-    }
+	err = models.DeleteRecipe(id)
+	if err != nil {
+		InternalServerError(ctx)
+		return
+	}
 
-    ctx.Redirect("/recipes")
+	ctx.Redirect("/recipes")
 }
