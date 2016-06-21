08cc2ee3c6303410110e7bc718c9a75fa989f7d6
Chad Weimer <chadweimer@gmail.com>
2016-06-20 21:53:02 -0700

    Usernames must now be unique, which should have been in place all along.

--- db/migrations/postgres/0002_authentication.up.sql
+++ db/migrations/postgres/0002_authentication.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE app_user (
     id SERIAL NOT NULL PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
 CREATE INDEX user_username_idx ON app_user(username);
