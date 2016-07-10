3441b3430fa42054d9f9d1552811d2c4ce4e87eb
Chad Weimer <chadweimer@gmail.com>
2016-07-10 14:30:53 -0700

    Added several additional categries to the home screen. Long term it will be nice to make this configurable in some way.

--- routers/home.go
+++ routers/home.go
@@ -12,29 +12,68 @@ import (
 func (rc *RouteController) Home(resp http.ResponseWriter, req *http.Request, p httprouter.Params) {
 	ctx := context.Get(req)
 
-	dinnerRecipes, dinnerCount, err := rc.model.Search.Find(
+	dinnerRecipes, _, err := rc.model.Search.Find(
 		models.SearchFilter{Tags: []string{"dinner"}, SortBy: models.SortByRandom}, 1, 6)
 	if rc.HasError(resp, req, err) {
 		return
 	}
 	ctx.Data["DinnerRecipes"] = dinnerRecipes
-	ctx.Data["DinnerRecipesCount"] = dinnerCount
 
-	lunchRecipes, lunchCount, err := rc.model.Search.Find(
+	lunchRecipes, _, err := rc.model.Search.Find(
 		models.SearchFilter{Tags: []string{"lunch"}, SortBy: models.SortByRandom}, 1, 6)
 	if rc.HasError(resp, req, err) {
 		return
 	}
 	ctx.Data["LunchRecipes"] = lunchRecipes
-	ctx.Data["LunchRecipesCount"] = lunchCount
 
-	breakfastRecipes, breakfastCount, err := rc.model.Search.Find(
+	breakfastRecipes, _, err := rc.model.Search.Find(
 		models.SearchFilter{Tags: []string{"breakfast"}, SortBy: models.SortByRandom}, 1, 6)
 	if rc.HasError(resp, req, err) {
 		return
 	}
 	ctx.Data["BreakfastRecipes"] = breakfastRecipes
-	ctx.Data["BreakfastRecipesCount"] = breakfastCount
+
+	drinkRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"drink", "cocktail"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["DrinkRecipes"] = drinkRecipes
+
+	beefRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"beef", "steak"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["BeefRecipes"] = beefRecipes
+
+	poultryRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"poultry", "chicken", "turkey"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["PoultryRecipes"] = poultryRecipes
+
+	porkRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"pork"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["PorkRecipes"] = porkRecipes
+
+	seafoodRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"seafood", "fish"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["SeafoodRecipes"] = seafoodRecipes
+
+	vegetarianRecipes, _, err := rc.model.Search.Find(
+		models.SearchFilter{Tags: []string{"vegetarian"}, SortBy: models.SortByRandom}, 1, 6)
+	if rc.HasError(resp, req, err) {
+		return
+	}
+	ctx.Data["VegetarianRecipes"] = vegetarianRecipes
 
 	ctx.Data["HomeTitle"] = rc.cfg.HomeTitle
 	ctx.Data["HomeImage"] = rc.cfg.HomeImage
