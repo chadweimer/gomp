ac5b923ec34de79a5672fa893e87b3e55870cebd
Chad Weimer <cweimer@illumina.com>
2016-08-09 12:22:16 -0700

    Moved ajax methods into shared javascript file

--- public/js/gomp.js
+++ public/js/gomp.js
@@ -4,3 +4,75 @@ $(document).ready(function(){
     $('.modal-trigger').leanModal();
     $('.dropdown').dropdown();
 });
+
+function loadRecipeAsync(rootUrlPath, recipeId) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId,
+        method: 'GET',
+        dataType: 'json',
+    });
+}
+
+function loadImagesAsync(rootUrlPath, recipeId) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/images',
+        method: 'GET',
+        dataType: 'json',
+    });
+}
+
+function loadNotesAsync(rootUrlPath, recipeId) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/notes',
+        method: 'GET',
+        dataType: 'json',
+    });
+}
+
+function addNoteAsync(rootUrlPath, recipeId, text) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/notes',
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'text',
+        processData: false,
+        data: JSON.stringify({
+            recipeId: recipeId,
+            text: text,
+        }),
+    });
+}
+
+function editNoteAsync(rootUrlPath, recipeId, noteId, text) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/notes/' + noteId,
+        method: 'PUT',
+        contentType: 'application/json',
+        dataType: 'text',
+        processData: false,
+        data: JSON.stringify({
+            id: noteId,
+            recipeId: recipeId,
+            text: text,
+        }),
+    });
+}
+
+function deleteNoteAsync(rootUrlPath, noteId) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/notes/' + noteId,
+        method: 'DELETE',
+        contentType: 'application/json',
+        dataType: 'text',
+    });
+}
+
+function editRatingAsync(rootUrlPath, recipeId, rating) {
+    return $.ajax({
+        url: rootUrlPath + '/api/v1/recipes/' + recipeId + '/rating',
+        method: 'PUT',
+        dataType: 'json',
+        processData: false,
+        data: rating,
+    });
+}
