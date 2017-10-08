1b387f6f1b1794b85a8bd7e16d8c628c15664a3e
Chad Weimer <chadweimer@gmail.com>
2017-10-08 16:31:37 -0700

    WIP for multi-step directions database schema

---
+++ db/migrations/postgres/0008_multi_step_directions.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE recipe_step (
+    id SERIAL NOT NULL PRIMARY KEY,
+    recipe_id INTEGER NOT NULL,
+    step_number INTEGER NOT NULL,
+    directions TEXT NOT NULL,
+    UNIQUE (recipe_id, step_number),
+    FOREIGN KEY(recipe_id) REFERENCES recipe(id)
+);
+CREATE INDEX recipe_step_directions_idx ON recipe_step(directions);
+
+INSERT INTO recipe_step (recipe_id, step_number, directions)
+SELECT recipe.id, 1, recipe.directions FROM recipe;
+
+ALTER TABLE recipe
+DROP COLUMN directions;
