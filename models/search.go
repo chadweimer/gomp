57232de0e66ca570a0402b6350d84bd7cfaedf81
Chad Weimer <cweimer@illumina.com>
2017-10-10 18:02:39 -0700

    First pass at implementing full test search It seems to be working quite well, but more testing to be done

--- models/search.go
+++ models/search.go
@@ -57,69 +57,81 @@ type TagsFilter struct {
 // FindRecipes retrieves all recipes matching the specified search filter and within the range specified.
 func (m *SearchModel) FindRecipes(filter RecipesFilter) (*Recipes, int64, error) {
 	var total int64
-	var search string
-	if filter.Query == "" {
-		search = "%"
-	} else {
-		search = "%" + filter.Query + "%"
-	}
-	partialStmt := "FROM recipe AS r " +
-		"WHERE (r.name ILIKE ? OR r.Ingredients ILIKE ? OR r.directions ILIKE ? OR EXISTS (SELECT 1 FROM recipe_tag as t WHERE t.recipe_id = r.id AND t.tag ILIKE ?))"
-	if len(filter.Tags) > 0 {
-		partialStmt += " AND EXISTS (SELECT 1 FROM recipe_tag AS t WHERE t.recipe_id = r.id AND t.tag IN (?))"
+	var err error
+
+	fromStmt := "FROM recipe AS r"
+
+	queryStmt := ""
+	queryArgs := make([]interface{}, 0)
+	if filter.Query != "" {
+		var err error
+		queryStmt, queryArgs, err = sqlx.In("to_tsvector('english', r.name || ' ' || r.ingredients || ' ' || r.directions) @@ plainto_tsquery(?)", filter.Query)
+		if err != nil {
+			return nil, 0, err
+		}
 	}
 
-	countStmt := "SELECT count(r.id) " + partialStmt
-	var err error
-	var countArgs []interface{}
-	if len(filter.Tags) == 0 {
-		countStmt, countArgs, err = sqlx.In(countStmt, search, search, search, search)
-	} else {
-		countStmt, countArgs, err = sqlx.In(countStmt, search, search, search, search, filter.Tags)
+	tagsStmt := ""
+	tagsArgs := make([]interface{}, 0)
+	if len(filter.Tags) > 0 {
+		tagsStmt, tagsArgs, err = sqlx.In("EXISTS (SELECT 1 FROM recipe_tag AS t WHERE t.recipe_id = r.id AND t.tag IN (?))", filter.Tags)
+		if err != nil {
+			return nil, 0, err
+		}
 	}
-	if err != nil {
-		return nil, 0, err
+
+	whereStmt := ""
+	whereArgs := append(queryArgs, tagsArgs...)
+	if queryStmt != "" {
+		whereStmt += " WHERE " + queryStmt
 	}
-	countStmt = m.db.Rebind(countStmt)
-	if err := m.db.Get(&total, countStmt, countArgs...); err != nil {
+	if tagsStmt != "" {
+		if whereStmt == "" {
+			whereStmt += " WHERE " + tagsStmt
+		} else {
+			whereStmt += " AND " + tagsStmt
+		}
+	}
+
+	countStmt := m.db.Rebind("SELECT count(r.id) " + fromStmt + whereStmt)
+	if err := m.db.Get(&total, countStmt, whereArgs...); err != nil {
 		return nil, 0, err
 	}
 
 	offset := filter.Count * (filter.Page - 1)
-	selectStmt := "SELECT " +
-		"r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating, COALESCE((SELECT thumbnail_url FROM recipe_image WHERE id = r.image_id), '')" +
-		partialStmt
+
+	var orderStmt string
+	var orderArgs []interface{}
 	switch filter.SortBy {
 	case SortRecipeByID:
-		selectStmt += " ORDER BY r.id"
+		orderStmt += " ORDER BY r.id"
 	case SortRecipeByRating:
-		selectStmt += " ORDER BY avg_rating"
+		orderStmt += " ORDER BY avg_rating"
 	case SortByRandom:
-		selectStmt += " ORDER BY RANDOM()"
+		orderStmt += " ORDER BY RANDOM()"
 	case SortRecipeByName:
 		fallthrough
 	default:
-		selectStmt += " ORDER BY r.name"
+		orderStmt += " ORDER BY r.name"
 	}
 	switch filter.SortDir {
 	case SortDirDesc:
-		selectStmt += " DESC"
+		orderStmt += " DESC"
 	case SortDirAsc:
 		fallthrough
 	default:
-		selectStmt += " ASC"
+		orderStmt += " ASC"
 	}
-	selectStmt += " LIMIT ? OFFSET ?"
-	var selectArgs []interface{}
-	if len(filter.Tags) == 0 {
-		selectStmt, selectArgs, err = sqlx.In(selectStmt, search, search, search, search, filter.Count, offset)
-	} else {
-		selectStmt, selectArgs, err = sqlx.In(selectStmt, search, search, search, search, filter.Tags, filter.Count, offset)
-	}
+	orderStmt += " LIMIT ? OFFSET ?"
+	orderStmt, orderArgs, err = sqlx.In(orderStmt, filter.Count, offset)
 	if err != nil {
 		return nil, 0, err
 	}
-	selectStmt = m.db.Rebind(selectStmt)
+
+	selectStmt := m.db.Rebind("SELECT " +
+		"r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating, COALESCE((SELECT thumbnail_url FROM recipe_image WHERE id = r.image_id), '')" +
+		fromStmt + whereStmt + orderStmt)
+	selectArgs := append(whereArgs, orderArgs...)
 	rows, err := m.db.Query(selectStmt, selectArgs...)
 	if err != nil {
 		return nil, 0, err
