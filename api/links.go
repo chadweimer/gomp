d341f004412c1756d4f33afbbbd933f2743885dc
Chad Weimer <chadweimer@gmail.com>
2018-06-13 21:56:15 -0700

    Stubbed out api layer for links. Won't yet compile

---
+++ api/links.go
@@ -0,0 +1,69 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/chadweimer/gomp/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (h apiHandler) getRecipeLinks(resp http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	recipeID, err := strconv.ParseInt(p.ByName("recipeID"), 10, 64)
+	if err != nil {
+		h.JSON(resp, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	recipes, err := h.model.Recipes.ListLinks(recipeID)
+	if err != nil {
+		h.JSON(resp, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	h.JSON(resp, http.StatusOK, recipes)
+}
+
+func (h apiHandler) postRecipeLink(resp http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	recipeID, err := strconv.ParseInt(p.ByName("recipeID")), 10, 64)
+	if err != nil {
+		h.JSON(resp, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var destRecipeID int64
+	if err := readJsonFromRequest(req, &destRecipeID); err != nil {
+		h.JSON(resp, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.model.Recipes.CreateLink(recipeID, destRecipeID); err != nil {
+		h.JSON(resp, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	resp.Header().Set("Location", fmt.Sprintf("/api/v1/recipes/%d/links/%d", recipeID, destRecipeID))
+	resp.WriteHeader(http.StatusCreated)
+}
+
+func (h apiHandler) deleteRecipeLink(resp http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	recipeID, err := strconv.ParseInt(p.ByName("recipeID"), 10, 64)
+	if err != nil {
+		h.JSON(resp, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	destRecipeID, err := strconv.ParseInt(p.ByName("destRecipeID"), 10, 64)
+	if err != nil {
+		h.JSON(resp, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.model.Recipes.DeleteLink(recipeID, destRecipeID); err != nil {
+		h.JSON(resp, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	resp.WriteHeader(http.StatusOK)
+}
