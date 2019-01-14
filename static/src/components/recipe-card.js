c19091f1310c13dcfdbbb84733cae6d3ffbac858
Chad Weimer <chadweimer@gmail.com>
2019-01-13 21:41:35 -0800

    De-linting

--- static/src/components/recipe-card.js
+++ static/src/components/recipe-card.js
@@ -1,69 +1,68 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { GestureEventListeners } from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import '@polymer/paper-card/paper-card.js';
 import '../mixins/gomp-core-mixin.js';
 import './recipe-rating.js';
 import '../shared-styles.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 class RecipeCard extends GompCoreMixin(GestureEventListeners(PolymerElement)) {
-  static get template() {
-    return html`
-        <style include="shared-styles">
-            :host {
-                display: block;
-                color: var(--primary-text-color);
-                cursor: pointer;
+    static get template() {
+        return html`
+            <style include="shared-styles">
+                :host {
+                    display: block;
+                    color: var(--primary-text-color);
+                    cursor: pointer;
 
-                --paper-card-header: {
-                    height: 75%;
+                    --paper-card-header: {
+                        height: 75%;
 
-                    @apply --recipe-card-header;
-                }
-                --paper-card-header-image: {
-                    margin-top: -25%;
-                }
-                --paper-card-content: {
-                    @apply --recipe-card-content;
-                }
-                --paper-card: {
-                    width: 100%;
-                    height: 250px;
+                        @apply --recipe-card-header;
+                    }
+                    --paper-card-header-image: {
+                        margin-top: -25%;
+                    }
+                    --paper-card-content: {
+                        @apply --recipe-card-content;
+                    }
+                    --paper-card: {
+                        width: 100%;
+                        height: 250px;
 
-                    @apply --recipe-card;
+                        @apply --recipe-card;
+                    }
+                    --recipe-rating-size: var(--recipe-card-rating-size, 18px);
                 }
-                --recipe-rating-size: var(--recipe-card-rating-size, 18px);
-            }
-            paper-card:hover {
-                @apply --shadow-elevation-6dp;
-            }
-            .truncate {
-                display: block;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-            }
-        </style>
+                paper-card:hover {
+                    @apply --shadow-elevation-6dp;
+                }
+                .truncate {
+                    display: block;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+          </style>
 
-        <a href\$="/recipes/[[recipe.id]]">
-            <paper-card image="[[recipe.thumbnailUrl]]">
-                <div class="card-content">
-                    <span class="truncate">[[recipe.name]]</span>
-                    <recipe-rating recipe="{{recipe}}"></recipe-rating>
-                </div>
-            </paper-card>
-        </a>
+          <a href\$="/recipes/[[recipe.id]]">
+              <paper-card image="[[recipe.thumbnailUrl]]">
+                  <div class="card-content">
+                      <span class="truncate">[[recipe.name]]</span>
+                      <recipe-rating recipe="{{recipe}}"></recipe-rating>
+                  </div>
+              </paper-card>
+          </a>
 `;
-  }
+    }
 
-  static get is() { return 'recipe-card'; }
-  static get properties() {
-      return {
-          recipe: {
-              type: Object,
-              notify: true,
-          },
-      };
-  }
+    static get is() { return 'recipe-card'; }
+    static get properties() {
+        return {
+            recipe: {
+                type: Object,
+                notify: true,
+            },
+        };
+    }
 }
 
 window.customElements.define(RecipeCard.is, RecipeCard);
--- static/src/components/recipe-edit.js
+++ static/src/components/recipe-edit.js
@@ -1,4 +1,4 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { GestureEventListeners } from '@polymer/polymer/lib/mixins/gesture-event-listeners.js';
 import '@polymer/iron-ajax/iron-ajax.js';
 import '@polymer/iron-input/iron-input.js';
@@ -11,153 +11,152 @@ import '@polymer/paper-spinner/paper-spinner.js';
 import '../mixins/gomp-core-mixin.js';
 import './tag-input.js';
 import '../shared-styles.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 class RecipeEdit extends GompCoreMixin(GestureEventListeners(PolymerElement)) {
-  static get template() {
-    return html`
-        <style include="shared-styles">
-            :host {
-                display: block;
+    static get template() {
+        return html`
+            <style include="shared-styles">
+                :host {
+                    display: block;
 
-                --paper-card: {
-                    width: 100%;
+                    --paper-card: {
+                        width: 100%;
+                    }
                 }
-            }
-        </style>
+          </style>
 
-        <paper-card>
-            <div class="card-content">
-                <paper-input label="Name" always-float-label="" value="{{recipe.name}}"></paper-input>
-                <form id="mainImageForm" enctype="multipart/form-data">
-                    <paper-input-container hidden\$="[[recipeId]]" always-float-label="">
-                        <label slot="label">Picture</label>
-                        <iron-input slot="input">
-                            <input id="mainImage" name="file_content" type="file" accept=".jpg,.jpeg,.png">
-                        </iron-input>
-                    </paper-input-container>
-                </form>
-                <paper-textarea label="Serving Size" always-float-label="" value="{{recipe.servingSize}}"></paper-textarea>
-                <paper-textarea label="Ingredients" always-float-label="" value="{{recipe.ingredients}}"></paper-textarea>
-                <paper-textarea label="Directions" always-float-label="" value="{{recipe.directions}}"></paper-textarea>
-                <paper-textarea label="Nutrition" always-float-label="" value="{{recipe.nutritionInfo}}"></paper-textarea>
-                <paper-input label="Source" always-float-label="" value="{{recipe.sourceUrl}}"></paper-input>
-                <tag-input id="tagsInput" tags="{{recipe.tags}}"></tag-input>
-            </div>
-            <div class="card-actions">
-                <paper-button on-tap="_cancelButtonTapped">Cancel</paper-button>
-                <paper-button on-tap="_saveButtonTapped">Save</paper-button>
-            </div>
-        </paper-card>
-        <paper-dialog id="uploadingDialog" with-backdrop="">
-            <h3><paper-spinner active=""></paper-spinner>Uploading</h3>
-        </paper-dialog>
+          <paper-card>
+              <div class="card-content">
+                  <paper-input label="Name" always-float-label="" value="{{recipe.name}}"></paper-input>
+                  <form id="mainImageForm" enctype="multipart/form-data">
+                      <paper-input-container hidden\$="[[recipeId]]" always-float-label="">
+                          <label slot="label">Picture</label>
+                          <iron-input slot="input">
+                              <input id="mainImage" name="file_content" type="file" accept=".jpg,.jpeg,.png">
+                          </iron-input>
+                      </paper-input-container>
+                  </form>
+                  <paper-textarea label="Serving Size" always-float-label="" value="{{recipe.servingSize}}"></paper-textarea>
+                  <paper-textarea label="Ingredients" always-float-label="" value="{{recipe.ingredients}}"></paper-textarea>
+                  <paper-textarea label="Directions" always-float-label="" value="{{recipe.directions}}"></paper-textarea>
+                  <paper-textarea label="Nutrition" always-float-label="" value="{{recipe.nutritionInfo}}"></paper-textarea>
+                  <paper-input label="Source" always-float-label="" value="{{recipe.sourceUrl}}"></paper-input>
+                  <tag-input id="tagsInput" tags="{{recipe.tags}}"></tag-input>
+              </div>
+              <div class="card-actions">
+                  <paper-button on-tap="_cancelButtonTapped">Cancel</paper-button>
+                  <paper-button on-tap="_saveButtonTapped">Save</paper-button>
+              </div>
+          </paper-card>
+          <paper-dialog id="uploadingDialog" with-backdrop="">
+              <h3><paper-spinner active=""></paper-spinner>Uploading</h3>
+          </paper-dialog>
 
-        <iron-ajax bubbles="" auto="" id="getAjax" url="/api/v1/recipes/[[recipeId]]" on-request="_handleGetRecipeRequest" on-response="_handleGetRecipeResponse"></iron-ajax>
-        <iron-ajax bubbles="" id="putAjax" url="/api/v1/recipes/[[recipeId]]" method="PUT" on-response="_handlePutRecipeResponse"></iron-ajax>
-        <iron-ajax bubbles="" id="postAjax" url="/api/v1/recipes" method="POST" on-response="_handlePostRecipeResponse"></iron-ajax>
-        <iron-ajax bubbles="" id="addImageAjax" url="/api/v1/recipes/[[newRecipeId]]/images" method="POST" on-request="_handleAddImageRequest" on-response="_handleAddImageResponse" ,="" on-error="_handleAddImageResponse"></iron-ajax>
+          <iron-ajax bubbles="" auto="" id="getAjax" url="/api/v1/recipes/[[recipeId]]" on-request="_handleGetRecipeRequest" on-response="_handleGetRecipeResponse"></iron-ajax>
+          <iron-ajax bubbles="" id="putAjax" url="/api/v1/recipes/[[recipeId]]" method="PUT" on-response="_handlePutRecipeResponse"></iron-ajax>
+          <iron-ajax bubbles="" id="postAjax" url="/api/v1/recipes" method="POST" on-response="_handlePostRecipeResponse"></iron-ajax>
+          <iron-ajax bubbles="" id="addImageAjax" url="/api/v1/recipes/[[newRecipeId]]/images" method="POST" on-request="_handleAddImageRequest" on-response="_handleAddImageResponse" ,="" on-error="_handleAddImageResponse"></iron-ajax>
 `;
-  }
+    }
 
-  static get is() { return 'recipe-edit'; }
-  static get properties() {
-      return {
-          recipeId: {
-              type: String,
-          },
-      };
-  }
+    static get is() { return 'recipe-edit'; }
+    static get properties() {
+        return {
+            recipeId: {
+                type: String,
+            },
+        };
+    }
 
-  ready() {
-      super.ready();
+    ready() {
+        super.ready();
 
-      if (this.isActive) {
-          this.$.tagsInput.refresh();
-      }
-  }
-  refresh() {
-      if (!this.recipeId) {
-          return;
-      }
+        if (this.isActive) {
+            this.$.tagsInput.refresh();
+        }
+    }
+    refresh() {
+        if (!this.recipeId) {
+            return;
+        }
 
-      this.$.getAjax.generateRequest();
-      this.$.tagsInput.refresh();
-  }
+        this.$.getAjax.generateRequest();
+        this.$.tagsInput.refresh();
+    }
 
-  _isActiveChanged(isActive) {
-      this.newRecipeId = NaN;
-      this.$.mainImage.value = null;
-      if (!this.recipeId) {
-          this.recipe = {
-              name: '',
-              servingSize: '',
-              ingredients: '',
-              directions: '',
-              nutrition: '',
-              sourceUrl: '',
-              tags: [],
-          };
-      }
-      if (isActive && this.isReady) {
-          this.$.tagsInput.refresh();
-      }
-  }
-  _cancelButtonTapped(e) {
-      e.preventDefault();
+    _isActiveChanged(isActive) {
+        this.newRecipeId = NaN;
+        this.$.mainImage.value = null;
+        if (!this.recipeId) {
+            this.recipe = {
+                name: '',
+                servingSize: '',
+                ingredients: '',
+                directions: '',
+                nutrition: '',
+                sourceUrl: '',
+                tags: [],
+            };
+        }
+        if (isActive && this.isReady) {
+            this.$.tagsInput.refresh();
+        }
+    }
+    _cancelButtonTapped(e) {
+        e.preventDefault();
 
-      this.dispatchEvent(new CustomEvent('recipe-edit-cancel'));
-  }
-  _saveButtonTapped(e) {
-      e.preventDefault();
+        this.dispatchEvent(new CustomEvent('recipe-edit-cancel'));
+    }
+    _saveButtonTapped(e) {
+        e.preventDefault();
 
-      if (this.recipeId) {
-          this.$.putAjax.body = JSON.stringify(this.recipe);
-          this.$.putAjax.generateRequest();
-      } else {
-          this.$.postAjax.body = JSON.stringify(this.recipe);
-          this.$.postAjax.generateRequest();
-      }
-  }
-  _handleGetRecipeRequest(e) {
-      if (this.recipeId) {
-          this.recipe = null;
-      }
-  }
-  _handleGetRecipeResponse(e) {
-      this.recipe = e.detail.response;
-  }
-  _handlePutRecipeResponse(e) {
-      this._onSaveComplete();
-  }
-  _handlePostRecipeResponse(e) {
-      var temp = document.createElement('a');
-      temp.href = e.detail.xhr.getResponseHeader('Location');
-      var path = temp.pathname;
+        if (this.recipeId) {
+            this.$.putAjax.body = JSON.stringify(this.recipe);
+            this.$.putAjax.generateRequest();
+        } else {
+            this.$.postAjax.body = JSON.stringify(this.recipe);
+            this.$.postAjax.generateRequest();
+        }
+    }
+    _handleGetRecipeRequest(e) {
+        if (this.recipeId) {
+            this.recipe = null;
+        }
+    }
+    _handleGetRecipeResponse(e) {
+        this.recipe = e.detail.response;
+    }
+    _handlePutRecipeResponse(e) {
+        this._onSaveComplete();
+    }
+    _handlePostRecipeResponse(e) {
+        var temp = document.createElement('a');
+        temp.href = e.detail.xhr.getResponseHeader('Location');
+        var path = temp.pathname;
 
-      this.newRecipeId = NaN;
-      var newRecipeIdMatch = path.match(/\/api\/v1\/recipes\/(\d+)/);
-      if (newRecipeIdMatch) {
-          this.newRecipeId = parseInt(newRecipeIdMatch[1], 10);
-      }
+        this.newRecipeId = NaN;
+        var newRecipeIdMatch = path.match(/\/api\/v1\/recipes\/(\d+)/);
+        if (newRecipeIdMatch) {
+            this.newRecipeId = parseInt(newRecipeIdMatch[1], 10);
+        }
 
-      if (this.$.mainImage.value) {
-          this.$.addImageAjax.body = new FormData(this.$.mainImageForm);
-          this.$.addImageAjax.generateRequest();
-      } else {
-          this._onSaveComplete();
-      }
-  }
-  _handleAddImageRequest(e) {
-      this.$.uploadingDialog.open();
-  }
-  _handleAddImageResponse(e) {
-      this.$.uploadingDialog.close();
-      this._onSaveComplete();
-  }
-  _onSaveComplete() {
-      this.dispatchEvent(new CustomEvent('recipe-edit-save', {detail: this.newRecipeId ? {redirectUrl: '/recipes/' + this.newRecipeId} : null}));
-      this.dispatchEvent(new CustomEvent('recipes-modified', {bubbles: true, composed: true}));
-  }
+        if (this.$.mainImage.value) {
+            this.$.addImageAjax.body = new FormData(this.$.mainImageForm);
+            this.$.addImageAjax.generateRequest();
+        } else {
+            this._onSaveComplete();
+        }
+    }
+    _handleAddImageRequest(e) {
+        this.$.uploadingDialog.open();
+    }
+    _handleAddImageResponse(e) {
+        this.$.uploadingDialog.close();
+        this._onSaveComplete();
+    }
+    _onSaveComplete() {
+        this.dispatchEvent(new CustomEvent('recipe-edit-save', {detail: this.newRecipeId ? {redirectUrl: '/recipes/' + this.newRecipeId} : null}));
+        this.dispatchEvent(new CustomEvent('recipes-modified', {bubbles: true, composed: true}));
+    }
 }
 
 window.customElements.define(RecipeEdit.is, RecipeEdit);
