f694a287052f9952d0199b5057ce5f00025d02a8
Chad Weimer <chadweimer@gmail.com>
2019-01-20 21:51:16 -0800

    Fixed type

--- static/src/components/recipe-card.ts
+++ static/src/components/recipe-card.ts
@@ -56,6 +56,6 @@ export class RecipeCard extends GompCoreMixin(PolymerElement) {
 `;
     }
 
-    @property({type: String, notify: true})
-    recipe = '';
+    @property({type: Object, notify: true})
+    recipe = null;
 }
