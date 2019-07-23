2e0a83153e2491194c3da8566b74c8edb9857855
Chad Weimer <chadweimer@gmail.com>
2019-07-23 07:04:42 -0700

    WIP for supporting middleware

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -21,10 +23,14 @@ func New() *Router {
 }
 
 func (r *RouterGroup) Handle(method, path string, handle httprouter.Handle) {
+	// Build out the full route path from the prefix
 	fullPath := path
 	if len(r.prefix) > 0 {
 		fullPath = r.prefix + path
 	}
+
+	// TODO: Attach all the middlewares
+
 	r.hr.Handle(method, fullPath, handle)
 }
 
@@ -33,11 +39,13 @@ func (r *RouterGroup) PUT(path string, handle httprouter.Handle) { r.Handle("PUT
 func (r *RouterGroup) POST(path string, handle httprouter.Handle) { r.Handle("POST", path, handle) }
 func (r *RouterGroup) DELETE(path string, handle httprouter.Handle) { r.Handle("DELETE", path, handle) }
 
-type GroupFunc func(g *RouterGroup)
+type GroupFunc func(*RouterGroup)
+type Middleware func(http.ResponseWriter, *http.Request, Middleware)
 
 type RouterGroup struct {
 	hr *httprouter.Router
 	prefix string
+	middlewares []Middleware
 }
 
 func (r *RouterGroup) NewGroup(path string) *RouterGroup {
@@ -48,10 +56,16 @@ func (r *RouterGroup) NewGroup(path string) *RouterGroup {
 	return &RouterGroup {
 		hr:     r.hr,
 		prefix: fullPath,
+		// TODO: Need to make a fopy
+		middlewares: r.middlewares,
 	}
 }
 
 func (r *RouterGroup) Group(path string, group GroupFunc) {
 	g := r.NewGroup(path)
 	group(g)
+}
+
+func(r *RouterGroup) Use(m Middleware) {
+	r.middlewares = append(r.middlewares, m)
 }
