e6eb6c6cb1b0a757a0084cf62d3c6289a7c36c1b
Chad Weimer <chadweimer@gmail.com>
2019-07-23 08:48:07 -0700

    More progress on middleware support. Still incomplete

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -22,30 +22,11 @@ func New() *Router {
 	}
 }
 
-func (r *RouterGroup) Handle(method, path string, handle httprouter.Handle) {
-	// Build out the full route path from the prefix
-	fullPath := path
-	if len(r.prefix) > 0 {
-		fullPath = r.prefix + path
-	}
-
-	// TODO: Attach all the middlewares
-
-	r.hr.Handle(method, fullPath, handle)
-}
-
-func (r *RouterGroup) GET(path string, handle httprouter.Handle) { r.Handle("GET", path, handle) }
-func (r *RouterGroup) PUT(path string, handle httprouter.Handle) { r.Handle("PUT", path, handle) }
-func (r *RouterGroup) POST(path string, handle httprouter.Handle) { r.Handle("POST", path, handle) }
-func (r *RouterGroup) DELETE(path string, handle httprouter.Handle) { r.Handle("DELETE", path, handle) }
-
 type GroupFunc func(*RouterGroup)
-type Middleware func(http.ResponseWriter, *http.Request, Middleware)
 
 type RouterGroup struct {
 	hr *httprouter.Router
 	prefix string
-	middlewares []Middleware
 }
 
 func (r *RouterGroup) NewGroup(path string) *RouterGroup {
@@ -66,6 +47,34 @@ func (r *RouterGroup) Group(path string, group GroupFunc) {
 	group(g)
 }
 
-func(r *RouterGroup) Use(m Middleware) {
-	r.middlewares = append(r.middlewares, m)
+func (r *RouterGroup) Handle(method, path string, handle httprouter.Handle) {
+	// Build out the full route path from the prefix
+	fullPath := path
+	if len(r.prefix) > 0 {
+		fullPath = r.prefix + path
+	}
+
+	// TODO: Attach all the middlewares
+
+	r.hr.Handle(method, fullPath, handle)
 }
+
+type MiddlewareFunc func(rw http.ResponseWriter, r *http.Request, next func(rw http.ResponseWriter, r *http.Request))
+
+type middleware struct {
+	handler MiddlewareFunc
+	next func(rw http.ResponseWriter, r *http.Request)
+}
+
+func (m middleware) Handler(rw http.ResponseWriter, r *http.Request) {
+	m.handler(rw, r, m.next)
+}
+
+func(r *RouterGroup) Use(f MiddlwareFunc) {
+	// TODO: Build middleware chain
+}
+
+func (r *RouterGroup) GET(path string, handle httprouter.Handle) { r.Handle("GET", path, handle) }
+func (r *RouterGroup) PUT(path string, handle httprouter.Handle) { r.Handle("PUT", path, handle) }
+func (r *RouterGroup) POST(path string, handle httprouter.Handle) { r.Handle("POST", path, handle) }
+func (r *RouterGroup) DELETE(path string, handle httprouter.Handle) { r.Handle("DELETE", path, handle) }
