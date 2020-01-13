6ac5e9261b905c475fd82b11125d9c6e0b321254
Chad Weimer <chadweimer@gmail.com>
2020-01-13 07:00:38 -0800

    Added created_at and modified_at to recipes

--- models/recipe.go
+++ models/recipe.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -13,19 +15,21 @@ type RecipeModel struct {
 
 // Recipe is the primary model class for recipe storage and retrieval
 type Recipe struct {
-	ID            int64    `json:"id" db:"id"`
-	Name          string   `json:"name" db:"name"`
-	ServingSize   string   `json:"servingSize" db:"serving_size"`
-	NutritionInfo string   `json:"nutritionInfo" db:"nutrition_info"`
-	Ingredients   string   `json:"ingredients" db:"ingredients"`
-	Directions    string   `json:"directions" db:"directions"`
-	SourceURL     string   `json:"sourceUrl" db:"source_url"`
-	AvgRating     float64  `json:"averageRating" db:"avg_rating"`
-	Tags          []string `json:"tags"`
+	ID            int64     `json:"id" db:"id"`
+	Name          string    `json:"name" db:"name"`
+	ServingSize   string    `json:"servingSize" db:"serving_size"`
+	NutritionInfo string    `json:"nutritionInfo" db:"nutrition_info"`
+	Ingredients   string    `json:"ingredients" db:"ingredients"`
+	Directions    string    `json:"directions" db:"directions"`
+	SourceURL     string    `json:"sourceUrl" db:"source_url"`
+	CreatedAt     time.Time `json:"createdAt" db:"created_at"`
+	ModifiedAt    time.Time `json:"modifiedAt" db:"modified_at"`
+	AvgRating     float64   `json:"averageRating" db:"avg_rating"`
+	Tags          []string  `json:"tags"`
 }
 
 // Create stores the recipe in the database as a new record using
-// a dedicated transation that is committed if there are not errors.
+// a dedicated transaction that is committed if there are not errors.
 func (m *RecipeModel) Create(recipe *Recipe) error {
 	return m.tx(func(tx *sqlx.Tx) error {
 		return m.CreateTx(recipe, tx)
@@ -41,13 +45,13 @@ func (m *RecipeModel) CreateTx(recipe *Recipe, tx *sqlx.Tx) error {
 	err := tx.Get(recipe, stmt,
 		recipe.Name, recipe.ServingSize, recipe.NutritionInfo, recipe.Ingredients, recipe.Directions, recipe.SourceURL)
 	if err != nil {
-		return err
+		return fmt.Errorf("creating recipe: %v", err)
 	}
 
 	for _, tag := range recipe.Tags {
 		err := m.Tags.CreateTx(recipe.ID, tag, tx)
 		if err != nil {
-			return err
+			return fmt.Errorf("adding tags to new recipe: %v", err)
 		}
 	}
 
@@ -58,19 +62,19 @@ func (m *RecipeModel) CreateTx(recipe *Recipe, tx *sqlx.Tx) error {
 // If no recipe exists with the specified ID, a NoRecordFound error is returned.
 func (m *RecipeModel) Read(id int64) (*Recipe, error) {
 	stmt := "SELECT " +
-		"r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, r.source_url, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating " +
+		"r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, r.source_url, r.created_at, r.modified_at, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating " +
 		"FROM recipe AS r WHERE r.id = $1"
 	recipe := new(Recipe)
 	err := m.db.Get(recipe, stmt, id)
 	if err == sql.ErrNoRows {
 		return nil, ErrNotFound
 	} else if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("reading recipe: %v", err)
 	}
 
 	tags, err := m.Tags.List(id)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("reading tags for recipe: %v", err)
 	}
 	recipe.Tags = *tags
 
@@ -78,7 +82,7 @@ func (m *RecipeModel) Read(id int64) (*Recipe, error) {
 }
 
 // Update stores the specified recipe in the database by updating the
-// existing record with the specified id using a dedicated transation
+// existing record with the specified id using a dedicated transaction
 // that is committed if there are not errors.
 func (m *RecipeModel) Update(recipe *Recipe) error {
 	return m.tx(func(tx *sqlx.Tx) error {
@@ -87,33 +91,33 @@ func (m *RecipeModel) Update(recipe *Recipe) error {
 }
 
 // UpdateTx stores the specified recipe in the database by updating the
-// existing record with the sepcified id using the specified transaction.
+// existing record with the specified id using the specified transaction.
 func (m *RecipeModel) UpdateTx(recipe *Recipe, tx *sqlx.Tx) error {
 	_, err := tx.Exec(
 		"UPDATE recipe "+
-			"SET name = $1, serving_size = $2, nutrition_info = $3, ingredients = $4, directions = $5, source_url = $6 "+
+			"SET name = $1, serving_size = $2, nutrition_info = $3, ingredients = $4, directions = $5, source_url = $6, modified_at = transaction_timestamp() "+
 			"WHERE id = $7",
 		recipe.Name, recipe.ServingSize, recipe.NutritionInfo, recipe.Ingredients, recipe.Directions, recipe.SourceURL, recipe.ID)
 	if err != nil {
-		return err
+		return fmt.Errorf("updating recipe: %v", err)
 	}
 
-	// Deleting and recreating seems inefficent. Maybe make this smarter.
+	// Deleting and recreating seems inefficient. Maybe make this smarter.
 	err = m.Tags.DeleteAllTx(recipe.ID, tx)
 	if err != nil {
-		return err
+		return fmt.Errorf("deleting tags before updating on recipe: %v", err)
 	}
 	for _, tag := range recipe.Tags {
 		err = m.Tags.CreateTx(recipe.ID, tag, tx)
 		if err != nil {
-			return err
+			return fmt.Errorf("updating tags on recipe: %v", err)
 		}
 	}
 
 	return nil
 }
 
-// Delete removes the specified recipe from the database using a dedicated transation
+// Delete removes the specified recipe from the database using a dedicated transaction
 // that is committed if there are not errors. Note that this method does not delete
 // any attachments that we associated with the deleted recipe.
 func (m *RecipeModel) Delete(id int64) error {
@@ -126,7 +130,7 @@ func (m *RecipeModel) Delete(id int64) error {
 // Note that this method does not delete any attachments that we associated with the deleted recipe.
 func (m *RecipeModel) DeleteTx(id int64, tx *sqlx.Tx) error {
 	if _, err := tx.Exec("DELETE FROM recipe WHERE id = $1", id); err != nil {
-		return err
+		return fmt.Errorf("deleting recipe: %v", err)
 	}
 
 	// If we successfully deleted the recipe, delete all of it's attachments
@@ -140,23 +144,27 @@ func (m *RecipeModel) SetRating(id int64, rating float64) error {
 	if err == sql.ErrNoRows || count == 0 {
 		_, err = m.db.Exec(
 			"INSERT INTO recipe_rating (recipe_id, rating) VALUES ($1, $2)", id, rating)
-		return err
+		return fmt.Errorf("creating recipe rating: %v", err)
 	}
 
 	if err == nil {
 		_, err = m.db.Exec(
 			"UPDATE recipe_rating SET rating = $1 WHERE recipe_id = $2", rating, id)
+		if err != nil {
+			return fmt.Errorf("updating recipe rating: %v", err)
+		}
 	}
-	return err
+
+	return nil
 }
 
 // List retrieves all recipes.
 func (m *RecipeModel) List() (*[]RecipeCompact, error) {
 	var recipes []RecipeCompact
 
-	selectStmt := "SELECT r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, r.source_url, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating, COALESCE((SELECT thumbnail_url FROM recipe_image WHERE id = r.image_id), '') AS thumbnail_url FROM recipe as r"
+	selectStmt := "SELECT r.id, r.name, r.serving_size, r.nutrition_info, r.ingredients, r.directions, r.source_url, r.created_at, r.modified_at, COALESCE((SELECT g.rating FROM recipe_rating AS g WHERE g.recipe_id = r.id), 0) AS avg_rating, COALESCE((SELECT thumbnail_url FROM recipe_image WHERE id = r.image_id), '') AS thumbnail_url FROM recipe as r"
 	if err := m.db.Select(&recipes, selectStmt); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("listing recipes: %v", err)
 	}
 
 	return &recipes, nil
