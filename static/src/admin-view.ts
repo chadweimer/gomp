0043942ce48bd2c2054ce705caeabd1064c12157
Chad Weimer <ch@dweimer.com>
2020-07-16 13:02:35 -0700

    Stubbing out user list

--- static/src/admin-view.ts
+++ static/src/admin-view.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { html } from '@polymer/polymer/polymer-element.js';
 import { customElement } from '@polymer/decorators';
+import { IronAjaxElement } from '@polymer/iron-ajax';
 import { GompBaseElement } from './common/gomp-base-element.js';
 import '@polymer/paper-card/paper-card.js';
 import './shared-styles.js';
@@ -23,10 +24,39 @@ export class AdminView extends GompBaseElement {
             </style>
             <div class="container">
                 <paper-card>
-                    <div class="card-content"></div>
+                    <div class="card-content">
+                        <h3>Users</h3>
+                        <template is="dom-repeat" items="[[users]]">
+                            <div>[[item.username]] - [[item.accessLevel]]</div>
+                        </template>
+                    </div>
                     <div class="card-actions"></div>
                 </paper-card>
             </div>
+
+            <iron-ajax bubbles="" id="getUsersAjax" url="/api/v1/users" on-response="handleGetUsersResponse"></iron-ajax>
 `;
+    }
+
+    protected users: any[] = [];
+
+    private get getUsersAjax(): IronAjaxElement {
+        return this.$.getUsersAjax as IronAjaxElement;
+    }
+
+    public ready() {
+        super.ready();
+
+        if (this.isActive) {
+            this.refresh();
+        }
+    }
+
+    protected refresh() {
+        this.getUsersAjax.generateRequest();
+    }
+
+    protected handleGetUsersResponse(e: CustomEvent) {
+        this.users = e.detail.response;
     }
 }
