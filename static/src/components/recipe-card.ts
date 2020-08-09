594330d17c45b242c78c214c5d73c1d13c7f3153
Chad Weimer <cwmr@localhost.localdomain>
2020-08-09 12:23:27 -0700

    Implemented all but add to list

--- static/src/components/recipe-card.ts
+++ static/src/components/recipe-card.ts
@@ -1,12 +1,20 @@
 'use strict';
 import { html } from '@polymer/polymer/polymer-element.js';
 import {customElement, property } from '@polymer/decorators';
+import { IronAjaxElement } from '@polymer/iron-ajax/iron-ajax.js';
+import { PaperDialogElement } from '@polymer/paper-dialog';
 import { GompBaseElement } from '../common/gomp-base-element.js';
+import { ConfirmationDialog } from './confirmation-dialog.js';
 import { RecipeCompact } from '../models/models.js';
+import '@polymer/iron-ajax/iron-ajax.js';
 import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-dialog/paper-dialog.js';
+import '@polymer/paper-dialog-scrollable/paper-dialog-scrollable.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@polymer/paper-card/paper-card.js';
 import '@cwmr/paper-chip/paper-chip.js';
+import './confirmation-dialog.js';
 import './recipe-rating.js';
 import '../shared-styles.js';
 
@@ -49,6 +57,15 @@ export class RecipeCard extends GompBaseElement {
                 paper-card:hover {
                     @apply --shadow-elevation-6dp;
                 }
+                #confirmArchiveDialog {
+                    --confirmation-dialog-title-color: var(--paper-purple-500);
+                }
+                #confirmUnarchiveDialog {
+                    --confirmation-dialog-title-color: var(--paper-purple-500);
+                }
+                #confirmDeleteDialog {
+                    --confirmation-dialog-title-color: var(--paper-red-500);
+                }
                 .truncate {
                     display: block;
                     white-space: nowrap;
@@ -89,7 +106,8 @@ export class RecipeCard extends GompBaseElement {
                     </div>
                     <div class="card-actions">
                         <paper-icon-button icon="icons:create" on-click="onEdit"></paper-icon-button>
-                        <paper-icon-button icon="icons:archive" on-click="onArchive"></paper-icon-button>
+                        <paper-icon-button icon="icons:archive" on-click="onArchive" hidden\$="[[!areEqual(recipe.state, 'active')]]"></paper-icon-button>
+                        <paper-icon-button icon="icons:unarchive" on-click="onUnarchive" hidden\$="[[areEqual(recipe.state, 'active')]]"></paper-icon-button>
                         <paper-icon-button icon="icons:delete" on-click="onDelete"></paper-icon-button>
                         <paper-icon-button icon="icons:list" on-click="onAddToList"></paper-icon-button>
                     </div>
@@ -97,6 +115,23 @@ export class RecipeCard extends GompBaseElement {
             </a>
             <paper-chip class="state" hidden\$="[[areEqual(recipe.state, 'active')]]">[[recipe.state]]</paper-chip>
         </div>
+
+        <confirmation-dialog id="confirmArchiveDialog" icon="icons:archive" title="Archive Recipe?" message="Are you sure you want to archive this recipe?" on-confirmed="archiveRecipe"></confirmation-dialog>
+        <confirmation-dialog id="confirmUnarchiveDialog" icon="icons:unarchive" title="Unarchive Recipe?" message="Are you sure you want to unarchive this recipe?" on-confirmed="unarchiveRecipe"></confirmation-dialog>
+        <confirmation-dialog id="confirmDeleteDialog" icon="icons:delete" title="Delete Recipe?" message="Are you sure you want to delete this recipe?" on-confirmed="deleteRecipe"></confirmation-dialog>
+
+        <paper-dialog id="addToListDialog" on-iron-overlay-opened="addToListDialogOpened" on-iron-overlay-closed="addToListDialogClosed" with-backdrop="">
+            <h3>Add to List</h3>
+            <paper-dialog-scrollable>
+            </paper-dialog-scrollable>
+            <div class="buttons">
+                <paper-button dialog-dismiss="">Cancel</paper-button>
+                <paper-button dialog-confirm="">Apply</paper-button>
+            </div>
+        </paper-dialog>
+
+        <iron-ajax bubbles="" id="updateStateAjax" url="/api/v1/recipes/[[recipe.id]]/state" method="PUT" on-response="handleUpdateStateResponse"></iron-ajax>
+        <iron-ajax bubbles="" id="deleteAjax" url="/api/v1/recipes/[[recipe.id]]" method="DELETE" on-response="handleDeleteRecipeResponse"></iron-ajax>
 `;
     }
 
@@ -108,6 +143,25 @@ export class RecipeCard extends GompBaseElement {
 
     @property({type: Boolean, reflectToAttribute: true})
     public readonly = false;
+
+    private get confirmArchiveDialog(): ConfirmationDialog {
+        return this.$.confirmArchiveDialog as ConfirmationDialog;
+    }
+    private get confirmUnarchiveDialog(): ConfirmationDialog {
+        return this.$.confirmUnarchiveDialog as ConfirmationDialog;
+    }
+    private get confirmDeleteDialog(): ConfirmationDialog {
+        return this.$.confirmDeleteDialog as ConfirmationDialog;
+    }
+    private get addToListDialog(): PaperDialogElement {
+        return this.$.addToListDialog as PaperDialogElement;
+    }
+    private get updateStateAjax(): IronAjaxElement {
+        return this.$.updateStateAjax as IronAjaxElement;
+    }
+    private get deleteAjax(): IronAjaxElement {
+        return this.$.deleteAjax as IronAjaxElement;
+    }
 
     protected formatDate(dateStr: string) {
         return new Date(dateStr).toLocaleDateString();
@@ -125,11 +179,48 @@ export class RecipeCard extends GompBaseElement {
     }
     protected onArchive(e: CustomEvent) {
         e.preventDefault();
+
+        this.confirmArchiveDialog.open();
+    }
+    protected onUnarchive(e: CustomEvent) {
+        e.preventDefault();
+
+        this.confirmUnarchiveDialog.open();
     }
     protected onDelete(e: CustomEvent) {
         e.preventDefault();
+
+        this.confirmDeleteDialog.open();
     }
     protected onAddToList(e: CustomEvent) {
         e.preventDefault();
+
+        this.addToListDialog.open();
+    }
+
+    protected archiveRecipe() {
+        this.updateStateAjax.body = JSON.stringify('archived') as any;
+        this.updateStateAjax.generateRequest();
+    }
+    protected unarchiveRecipe() {
+        this.updateStateAjax.body = JSON.stringify('active') as any;
+        this.updateStateAjax.generateRequest();
+    }
+    protected deleteRecipe() {
+        this.deleteAjax.generateRequest();
+    }
+
+    protected addToListDialogOpened() {
+        // TODO
+    }
+    protected addToListDialogClosed() {
+        // TODO
+    }
+
+    protected handleUpdateStateResponse() {
+        this.dispatchEvent(new CustomEvent('recipes-modified', {bubbles: true, composed: true}));
+    }
+    protected handleDeleteRecipeResponse() {
+        this.dispatchEvent(new CustomEvent('recipes-modified', {bubbles: true, composed: true}));
     }
 }
