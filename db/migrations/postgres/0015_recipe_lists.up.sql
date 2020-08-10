354099fa00e4dcda92271e7f3f58d6ef69c338d2
Chad Weimer <ch@dweimer.com>
2020-08-10 09:28:04 -0700

    Adjusted migration

--- db/migrations/postgres/0015_recipe_lists.up.sql
+++ db/migrations/postgres/0015_recipe_lists.up.sql
@@ -2,32 +2,29 @@ BEGIN;
 
 CREATE TABLE menu (
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE menu_recipe (
-    menu_id INTEGER NOT NULL,
-    recipe_id INTEGER NOT NULL,
-    FOREIGN KEY(menu_id) REFERENCES menu(id) ON DELETE CASCADE,
-    FOREIGN KEY(recipe_id) REFERENCES recipe(id) ON DELETE CASCADE
+    menu_id INTEGER NOT NULL REFERENCES menu(id) ON DELETE CASCADE,
+    recipe_id INTEGER NOT NULL REFERENCES recipe(id) ON DELETE CASCADE,
+    PRIMARY KEY (menu_id, recipe_id)
 );
-CREATE INDEX menu_recipe_menu_id_idx ON menu(id);
-CREATE INDEX menu_recipe_recipe_id_idx ON recipe(id);
 
+-- TODO: Make lists per user?
 CREATE TABLE recipe_list (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    modified_at TIMESTAMP WITH TIME ZONE NOT NULL
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE recipe_list_menu (
-    recipe_list_id INTEGER NOT NULL,
-    menu_id INTEGER NOT NULL,
-    FOREIGN KEY(recipe_list_id) REFERENCES recipe_list(id) ON DELETE CASCADE,
-    FOREIGN KEY(menu_id) REFERENCES menu(id) ON DELETE CASCADE
+    recipe_list_id INTEGER NOT NULL REFERENCES recipe_list(id) ON DELETE CASCADE,
+    menu_id INTEGER NOT NULL REFERENCES menu(id) ON DELETE CASCADE,
+    PRIMARY KEY(recipe_list_id, menu_id)
 );
-CREATE INDEX recipe_list_menu_recipe_list_id_idx ON recipe_list(id);
-CREATE INDEX recipe_list_menu_menu_id_idx ON menu(id);
 
 COMMIT;
