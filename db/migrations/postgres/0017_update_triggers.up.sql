00ac26162fdf1cc6ccf7ac18d7c5c94211f3b895
Chad Weimer <ch@dweimer.com>
2021-01-04 18:53:53 -0800

    Update recipe modified when changing tags

--- db/migrations/postgres/0017_update_triggers.up.sql
+++ db/migrations/postgres/0017_update_triggers.up.sql
@@ -15,6 +15,32 @@ CREATE TRIGGER on_recipe_update
     WHEN (OLD.* IS DISTINCT FROM NEW.*)
     EXECUTE FUNCTION on_recipe_update();
 
+CREATE FUNCTION on_recipe_tag_insert() RETURNS TRIGGER AS $$
+    BEGIN
+        UPDATE recipe SET modified_at = CURRENT_TIMESTAMP WHERE id = NEW.recipe_id;
+
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_recipe_tag_insert
+    AFTER INSERT ON recipe_tag
+    FOR EACH ROW
+    EXECUTE FUNCTION on_recipe_tag_insert();
+
+CREATE FUNCTION on_recipe_tag_delete() RETURNS TRIGGER AS $$
+    BEGIN
+        UPDATE recipe SET modified_at = CURRENT_TIMESTAMP WHERE id = OLD.recipe_id;
+
+        RETURN OLD;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_recipe_tag_delete
+    AFTER DELETE ON recipe_tag
+    FOR EACH ROW
+    EXECUTE FUNCTION on_recipe_tag_delete();
+
 -- recipe_note
 CREATE FUNCTION on_recipe_note_update() RETURNS TRIGGER AS $$
     BEGIN
--- db/migrations/sqlite3/0003_update_triggers.down.sql
+++ db/migrations/sqlite3/0003_update_triggers.down.sql
@@ -1,3 +1,5 @@
 DROP TRIGGER on_recipe_update;
+DROP TRIGGER on_recipe_tag_insert;
+DROP TRIGGER on_recipe_tag_delete;
 DROP TRIGGER on_recipe_note_update;
 DROP TRIGGER on_recipe_image_update;
