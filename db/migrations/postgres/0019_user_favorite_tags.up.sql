ddec9d9f82622330a2010297ba959a11bf375be4
Chad Weimer <ch@dweimer.com>
2021-01-31 15:13:56 -0800

    Add favorite tags for users

---
+++ db/migrations/postgres/0019_user_favorite_tags.up.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+CREATE TABLE app_user_favorite_tag (
+    user_id INTEGER NOT NULL,
+    tag TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(id) ON DELETE CASCADE
+);
+CREATE INDEX app_user_favorite_tag_idx ON app_user_favorite_tag(tag);
+CREATE INDEX app_user_favorite_tag_user_id_idx ON app_user_favorite_tag(user_id);
+
+COMMIT;
