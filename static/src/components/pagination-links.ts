ef8325fe4479c2ed935746fc1fe896dbafd36fe1
Chad Weimer <chadweimer@gmail.com>
2021-02-07 22:07:20 -0800

    Update pagination-links.ts

--- static/src/components/pagination-links.ts
+++ static/src/components/pagination-links.ts
@@ -1,7 +1,6 @@
 'use strict';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { customElement, property } from '@polymer/decorators';
-import { PaperButtonElement } from '@polymer/paper-button/paper-button.js';
 import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/paper-button/paper-button.js';
