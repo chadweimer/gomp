0d6843738e355a72c6f750cdf728f9d850c58716
Chad Weimer <chadweimer@gmail.com>
2021-02-07 21:27:21 -0800

    Update home-list.ts

--- static/src/components/home-list.ts
+++ static/src/components/home-list.ts
@@ -32,6 +32,9 @@ export class HomeList extends GompBaseElement {
                     }
                     --recipe-card-rating-size: 16px;
                 }
+                h3 {
+                    line-height: unset;
+                }
                 @media screen and (min-width: 992px) {
                     .recipeContainer {
                         width: 16.6%;
@@ -49,7 +52,7 @@ export class HomeList extends GompBaseElement {
                 }
             </style>
 
-            <h5><a href="#!" on-click="onLinkClicked">[[title]] ([[total]])</a></h5>
+            <h3><a href="#!" on-click="onLinkClicked">[[title]] ([[total]])</a></h3>
             <div class="wrap-horizontal">
                 <template is="dom-repeat" items="[[recipes]]">
                     <div class="recipeContainer">
