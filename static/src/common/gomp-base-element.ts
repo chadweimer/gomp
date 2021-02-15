d0d95bed3ecbee2862d560d1f7775cfed6009243
Chad Weimer <ch@dweimer.com>
2021-02-15 14:29:13 -0800

    Added ajax helper methods built atop the fetch API

--- static/src/common/gomp-base-element.ts
+++ static/src/common/gomp-base-element.ts
@@ -39,4 +39,55 @@ export abstract class GompBaseElement extends PolymerElement {
     protected formatDate(dateStr: string) {
         return new Date(dateStr).toLocaleDateString();
     }
+
+    protected async AjaxGet<TResult>(url: string, queryObj?: any) {
+        const query = new URLSearchParams(queryObj);
+        const fullUrl = `${url}?${query.toString()}`;
+
+        const init: RequestInit = {};
+        const resp = await this.ajaxFetch(fullUrl, init);
+        const result = await resp.json() as TResult;
+        return result;
+    }
+
+    protected async AjaxPut<TBody>(url: string, body: TBody) {
+        const init: RequestInit = {
+            method: 'PUT',
+            body: JSON.stringify(body)
+        };
+        await this.ajaxFetch(url, init);
+    }
+
+    protected async AjaxPost<TBody>(url: string, body: TBody) {
+        const init: RequestInit = {
+            method: 'POST',
+            body: JSON.stringify(body)
+        };
+        const resp = await this.ajaxFetch(url, init);
+        return resp.headers.get('Location');
+    }
+
+    protected async AjaxDelete(url: string) {
+        const init: RequestInit = {
+            method: 'DELETE'
+        };
+        await this.ajaxFetch(url, init);
+    }
+
+    private async ajaxFetch(url: string, init?: RequestInit) {
+        const shouldProceed = this.dispatchEvent(new CustomEvent('ajax-presend', {bubbles: true, composed: true, cancelable: true, detail: {options: init}}));
+        if (!shouldProceed) {
+            return null;
+        }
+
+        try {
+            const resp = await fetch(url, init);
+            this.dispatchEvent(new CustomEvent('ajax-response', {bubbles: true, composed: true, detail: resp}));
+
+            return resp;
+        } catch (e) {
+            this.dispatchEvent(new CustomEvent('ajax-error', {bubbles: true, composed: true, detail: e}));
+            throw e;
+        }
+    }
 }
