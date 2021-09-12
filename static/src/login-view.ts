68c3cedc3815342f46c8871cbf50b0f0fa103cc0
Chad Weimer <chadweimer@gmail.com>
2021-09-12 21:39:17 +0000

    Update login to use mwc inputs

--- static/src/login-view.ts
+++ static/src/login-view.ts
@@ -1,11 +1,11 @@
+import { TextField } from '@material/mwc-textfield';
 import { html } from '@polymer/polymer/polymer-element.js';
-import { customElement, property } from '@polymer/decorators';
+import { customElement, property, query } from '@polymer/decorators';
 import { GompBaseElement } from './common/gomp-base-element.js';
 import '@material/mwc-button';
+import '@material/mwc-textfield';
 import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import '@polymer/paper-card/paper-card.js';
-import '@polymer/paper-input/paper-input.js';
-import '@cwmr/paper-password-input/paper-password-input.js';
 import './common/shared-styles.js';
 
 @customElement('login-view')
@@ -30,8 +30,8 @@ export class LoginView extends GompBaseElement {
             <div class="padded-10 centered-horizontal">
                 <paper-card heading="Login">
                     <div class="card-content">
-                        <paper-input name="username" value="{{username}}" label="Email" type="email" on-keydown="onInputKeydown" required autofocus autocomplete></paper-input>
-                        <paper-password-input name="password" value="{{password}}" label="Password" on-keydown="onInputKeydown" required></paper-password-input>
+                        <p><mwc-textfield id="username" name="username" class="fill" label="Email" type="email" on-keydown="onInputKeydown" autofocus autocomplete></mwc-textfield></p>
+                        <p><mwc-textfield id="password" name="password" class="fill" label="Password" type="password" on-keydown="onInputKeydown" required></mwc-textfield></p>
                         <div class="red">[[errorMessage]]</div>
                     </div>
                     <div class="card-actions">
@@ -42,24 +42,44 @@ export class LoginView extends GompBaseElement {
 `;
     }
 
-    @property({type: String, notify: true})
-    public username = '';
-    @property({type: String, notify: true})
-    public password = '';
+    @query('#username')
+    private username!: TextField;
+    @query('#password')
+    private password!: TextField;
+
     @property({type: String, notify: true})
     public errorMessage = '';
 
     protected isActiveChanged(isActive: boolean) {
         if (isActive) {
-            this.username = '';
-            this.password = '';
+            this.username.value = '';
+            this.password.value = '';
             this.errorMessage = '';
         }
     }
     protected async onLoginClicked() {
+        const username = this.username.value.trim();
+        if (username === '') {
+            this.username.setCustomValidity('Username is required');
+            this.username.reportValidity();
+            return;
+        } else {
+            this.username.setCustomValidity('');
+            this.username.reportValidity();
+        }
+        const password = this.password.value.trim();
+        if (password === '') {
+            this.password.setCustomValidity('Password is required');
+            this.password.reportValidity();
+            return;
+        } else {
+            this.password.setCustomValidity('');
+            this.password.reportValidity();
+        }
+
         const authDetails = {
-            username: this.username,
-            password: this.password
+            username: username,
+            password: password
         };
         try {
             this.errorMessage = '';
@@ -68,7 +88,7 @@ export class LoginView extends GompBaseElement {
             this.dispatchEvent(new CustomEvent('authentication-changed', {bubbles: true, composed: true}));
             this.navigateTo('/home');
         } catch (e) {
-            this.password = '';
+            this.password.value = '';
             this.errorMessage = 'Login failed. Check your username and password and try again.';
             console.error(e);
         }
