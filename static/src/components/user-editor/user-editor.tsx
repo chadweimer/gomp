ede08ad99195776719f4c898216c9e10d0dabb5d
Chad Weimer <chadweimer@gmail.com>
2021-09-20 23:34:11 -0700

    Added controls

--- static/src/components/user-editor/user-editor.tsx
+++ static/src/components/user-editor/user-editor.tsx
@@ -9,7 +9,7 @@ export class UserEditor {
   @Element() el: HTMLElement;
 
   render() {
-    return (
+    return [
       <ion-header>
         <ion-toolbar>
           <ion-buttons slot="primary">
@@ -20,8 +20,27 @@ export class UserEditor {
             <ion-button color="danger" onClick={() => this.onCancelClicked()}>Cancel</ion-button>
           </ion-buttons>
         </ion-toolbar>
-      </ion-header>
-    );
+      </ion-header>,
+
+      <ion-content>
+        <ion-item>
+          <ion-label position="floating">Email</ion-label>
+          <ion-input />
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Access Level</ion-label>
+          <ion-select placeholder="Select One" />
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Password</ion-label>
+          <ion-input type="password" />
+        </ion-item>
+        <ion-item>
+          <ion-label position="floating">Confirm Password</ion-label>
+          <ion-input type="password" />
+        </ion-item>
+      </ion-content>
+    ];
   }
 
   onCancelClicked() {
