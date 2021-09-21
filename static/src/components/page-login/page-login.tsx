8717e420f1e70696f6057b4203cfa845eb81ee28
Chad Weimer <chadweimer@gmail.com>
2021-09-21 23:29:59 +0000

    Use grid for login

--- static/src/components/page-login/page-login.tsx
+++ static/src/components/page-login/page-login.tsx
@@ -13,33 +13,41 @@ export class PageLogin {
 
   render() {
     return (
-      <ion-card class="container">
-        <ion-card-header>
-          <ion-card-title>Login</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>
-          <ion-item>
-            <ion-label>Email</ion-label>
-            <ion-input value={this.email}
-              onIonChange={e => this.email = e.detail.value}
-              onKeyDown={e => this.onInputKeyDown(e)} />
-          </ion-item>
-          <ion-item>
-            <ion-icon slot="end" name="eye-off" />
-            <ion-label>Password</ion-label>
-            <ion-input type="password" value={this.password}
-              onIonChange={e => this.password = e.detail.value}
-              onKeyDown={e => this.onInputKeyDown(e)} />
-          </ion-item>
-        </ion-card-content>
-        <ion-footer>
-          <ion-toolbar>
-            <ion-buttons slot="primary">
-              <ion-button color="primary" onClick={() => this.onLoginClicked()}>Login</ion-button>
-            </ion-buttons>
-          </ion-toolbar>
-        </ion-footer>
-      </ion-card>
+      <ion-content>
+        <ion-grid class="no-pad">
+          <ion-row class="ion-justify-content-center">
+            <ion-col size-xs="12" size-sm="12" size-md="10" size-lg="8" size-xl="6">
+              <ion-card>
+                <ion-card-header>
+                  <ion-card-title>Login</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <ion-item>
+                    <ion-label>Email</ion-label>
+                    <ion-input value={this.email}
+                      onIonChange={e => this.email = e.detail.value}
+                      onKeyDown={e => this.onInputKeyDown(e)} />
+                  </ion-item>
+                  <ion-item>
+                    <ion-icon slot="end" name="eye-off" />
+                    <ion-label>Password</ion-label>
+                    <ion-input type="password" value={this.password}
+                      onIonChange={e => this.password = e.detail.value}
+                      onKeyDown={e => this.onInputKeyDown(e)} />
+                  </ion-item>
+                </ion-card-content>
+                <ion-footer>
+                  <ion-toolbar>
+                    <ion-buttons slot="primary">
+                      <ion-button color="primary" onClick={() => this.onLoginClicked()}>Login</ion-button>
+                    </ion-buttons>
+                  </ion-toolbar>
+                </ion-footer>
+              </ion-card>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-content>
     );
   }
 
