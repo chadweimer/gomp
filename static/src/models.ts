6f68ed2125e916f87f2d75da8282259df07ad471
Chad Weimer <chadweimer@gmail.com>
2021-09-27 00:09:41 +0000

    Major simplification

--- static/src/models.ts
+++ static/src/models.ts
@@ -1,150 +1,167 @@
 export enum SearchField {
-    Name = 'name',
-    Ingredients = 'ingredients',
-    Directions = 'directions'
+  Name = 'name',
+  Ingredients = 'ingredients',
+  Directions = 'directions'
 }
 
 export enum RecipeState {
-    Active = 'active',
-    Archived = 'archived',
+  Active = 'active',
+  Archived = 'archived',
 }
 
 export enum SearchPictures {
-    Yes = 'yes',
-    No = 'no',
-    Any = 'any'
+  Yes = 'yes',
+  No = 'no',
+  Any = 'any'
 }
 
 export enum SortBy {
-    Name = 'name',
-    Rating = 'rating',
-    Created = 'created',
-    Modified = 'modified',
-    Random = 'random'
+  Name = 'name',
+  Rating = 'rating',
+  Created = 'created',
+  Modified = 'modified',
+  Random = 'random'
 }
 
 export enum SortDir {
-    Asc = 'asc',
-    Desc = 'desc'
+  Asc = 'asc',
+  Desc = 'desc'
 }
 
 export enum AccessLevel {
-    Administrator = 'admin',
-    Editor = 'editor',
-    Viewer = 'viewer'
+  Administrator = 'admin',
+  Editor = 'editor',
+  Viewer = 'viewer'
+}
+
+export enum SearchViewMode {
+  Card = 'card',
+  List = 'list'
 }
 
 export class DefaultSearchFilter implements SearchFilter {
-    constructor() {
-        this.query = '';
-        this.withPictures = null;
-        this.fields = [SearchField.Name, SearchField.Ingredients, SearchField.Directions];
-        this.states = [RecipeState.Active];
-        this.tags = [];
-        this.sortBy = SortBy.Name;
-        this.sortDir = SortDir.Asc;
-    }
+  constructor() {
+    this.query = '';
+    this.withPictures = null;
+    this.fields = [SearchField.Name, SearchField.Ingredients, SearchField.Directions];
+    this.states = [RecipeState.Active];
+    this.tags = [];
+    this.sortBy = SortBy.Name;
+    this.sortDir = SortDir.Asc;
+  }
 
-    query: string;
-    withPictures: boolean|null;
-    fields: SearchField[];
-    states: RecipeState[];
-    tags: string[];
-    sortBy: SortBy;
-    sortDir: SortDir;
+  query: string;
+  withPictures: boolean | null;
+  fields: SearchField[];
+  states: RecipeState[];
+  tags: string[];
+  sortBy: SortBy;
+  sortDir: SortDir;
+}
+
+export class DefaultSearchSettings implements SearchSettings {
+  constructor() {
+    this.viewMode = SearchViewMode.Card;
+  }
+
+  viewMode: SearchViewMode;
 }
 
 export interface SavedSearchFilterCompact {
-    id?: number;
-    userId: number;
-    name: string;
+  id?: number;
+  userId: number;
+  name: string;
 }
 
 export interface SearchFilter {
-    query: string;
-    withPictures: boolean|null;
-    fields: SearchField[];
-    states: RecipeState[];
-    tags: string[];
-    sortBy: SortBy;
-    sortDir: SortDir;
+  query: string;
+  withPictures: boolean | null;
+  fields: SearchField[];
+  states: RecipeState[];
+  tags: string[];
+  sortBy: SortBy;
+  sortDir: SortDir;
 }
 
 export interface SavedSearchFilter extends SavedSearchFilterCompact, SearchFilter {
 }
 
+export interface SearchSettings {
+  viewMode: SearchViewMode;
+}
+
 export interface AppInfo {
-    version: string;
+  version: string;
 }
 
 export interface AppConfiguration {
-    title: string;
+  title: string;
 }
 
 export interface User {
-    id?: number;
-    username: string;
-    accessLevel: string;
+  id?: number;
+  username: string;
+  accessLevel: string;
 }
 
 export interface NewUser {
-    username: string;
-    accessLevel: string;
-    password: string;
+  username: string;
+  accessLevel: string;
+  password: string;
 }
 
 export interface UserSettings {
-    userId: string;
-    homeTitle: string;
-    homeImageUrl: string;
-    favoriteTags: string[];
+  userId: string;
+  homeTitle: string;
+  homeImageUrl: string;
+  favoriteTags: string[];
 }
 
 interface RecipeBase {
-    id?: number;
-    name: string;
-    state: string;
-    createdAt?: string;
-    modifiedAt?: string;
-    averageRating: number;
+  id?: number;
+  name: string;
+  state: string;
+  createdAt?: string;
+  modifiedAt?: string;
+  averageRating: number;
 }
 
 export interface Recipe extends RecipeBase {
-    servingSize: string;
-    nutritionInfo: string;
-    ingredients: string;
-    directions: string;
-    storageInstructions: string;
-    sourceUrl: string;
-    tags: string[];
+  servingSize: string;
+  nutritionInfo: string;
+  ingredients: string;
+  directions: string;
+  storageInstructions: string;
+  sourceUrl: string;
+  tags: string[];
 }
 
 export interface RecipeCompact extends RecipeBase {
-    thumbnailUrl: string;
+  thumbnailUrl: string;
 }
 
 export interface Note {
-    id?: number;
-    recipeId: number;
-    text: string;
-    createdAt?: string;
-    modifiedAt?: string;
+  id?: number;
+  recipeId: number;
+  text: string;
+  createdAt?: string;
+  modifiedAt?: string;
 }
 
 export interface RecipeImage {
-    id?: number;
-    recipeId: number;
-    name: string;
-    url: string;
-    thumbnailUrl: string;
-    createdAt?: string;
-    modifiedAt?: string;
+  id?: number;
+  recipeId: number;
+  name: string;
+  url: string;
+  thumbnailUrl: string;
+  createdAt?: string;
+  modifiedAt?: string;
 }
 
 export interface EventWithModel<T = any> extends Event {
-    model: T;
+  model: T;
 }
 
 export interface EventWithTarget<T extends EventTarget> extends Event {
-    target: T;
+  target: T;
 }
--- static/src/store.ts
+++ static/src/store.ts
@@ -1,68 +1,31 @@
-import { createStore, Subscription } from '@stencil/store';
-import { AppConfiguration, AppInfo, DefaultSearchFilter, SearchFilter, User } from './models';
+import { createStore } from '@stencil/store';
+import { AppConfiguration, AppInfo, DefaultSearchFilter, DefaultSearchSettings, SearchFilter, SearchSettings, User } from './models';
 
 interface AppState {
   appInfo: AppInfo;
   appConfig: AppConfiguration;
-  jwtToken?: string;
+  jwtToken: string;
   currentUser?: User;
-  search: SearchFilter;
+  searchFilter?: SearchFilter;
+  searchSettings: SearchSettings;
+  searchPage: number;
 }
 
-const { state, use } = createStore<AppState>({
+const { state, onChange } = createStore<AppState>({
   appInfo: {
     version: '<Unknown>'
   },
   appConfig: {
     title: 'GOMP: Go Meal Planner'
   },
-  search: new DefaultSearchFilter()
+  jwtToken: localStorage.getItem('jwtToken'),
+  searchFilter: sessionStorage.getItem('searchFilter') ? JSON.parse(sessionStorage.getItem('searchFilter')) : new DefaultSearchFilter(),
+  searchSettings: sessionStorage.getItem('searchSettings') ? JSON.parse(sessionStorage.getItem('searchSettings')) : new DefaultSearchSettings(),
+  searchPage: sessionStorage.getItem('searchPage') ? JSON.parse(sessionStorage.getItem('searchPage')) : 1
 });
 
-class StorageSync implements Subscription<AppState> {
-  get(key: keyof AppState) {
-    switch (key) {
-      case 'jwtToken':
-        state.jwtToken = localStorage.getItem(key);
-        break;
-
-      case 'search':
-        {
-          const sessionSearch = sessionStorage.getItem('search');
-          if (sessionSearch) {
-            state.search = JSON.parse(sessionSearch);
-          } else {
-            state.search = new DefaultSearchFilter();
-          }
-        }
-        break;
-    }
-  }
-  set(key: keyof AppState, newValue: AppState[keyof AppState]) {
-    switch (key) {
-      case 'jwtToken':
-        if (newValue) {
-          localStorage.setItem(key, newValue as string);
-        } else {
-          localStorage.removeItem(key);
-        }
-        break;
-
-      case 'search':
-        if (newValue) {
-          sessionStorage.setItem(key, JSON.stringify(newValue));
-        } else {
-          sessionStorage.setItem(key, JSON.stringify(new DefaultSearchFilter()));
-        }
-        break;
-    }
-  }
-  reset() {
-    localStorage.clear();
-    sessionStorage.clear();
-  }
-}
-
-use(new StorageSync());
+onChange('searchFilter', val => val ? sessionStorage.setItem('searchFilter', JSON.stringify(val)) : sessionStorage.removeItem('searchFilter'));
+onChange('searchSettings', val => val ? sessionStorage.setItem('searchSettings', JSON.stringify(val)) : sessionStorage.removeItem('searchSettings'));
+onChange('searchPage', val => val ? sessionStorage.setItem('searchPage', JSON.stringify(val)) : sessionStorage.removeItem('searchPage'));
 
 export default state;
