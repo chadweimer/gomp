4f61a1171be06392b3275e3b5d65076cfe2c1e86
Chad Weimer <chadweimer@gmail.com>
2022-07-04 16:40:18 -0700

    SQLite is funny like that

--- db/migrations/sqlite3/0007_user_dates.down.sql
+++ db/migrations/sqlite3/0007_user_dates.down.sql
@@ -1,5 +1,4 @@
-ALTER TABLE app_user
-DROP COLUMN created_at,
-DROP COLUMN modified_at;
+ALTER TABLE app_user DROP COLUMN created_at;
+ALTER TABLE app_user DROP COLUMN modified_at;
 
 DROP TRIGGER on_app_user_update;
--- db/migrations/sqlite3/0007_user_dates.up.sql
+++ db/migrations/sqlite3/0007_user_dates.up.sql
@@ -1,9 +1,28 @@
-ALTER TABLE app_user
-ADD COLUMN created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN modified_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+PRAGMA foreign_keys=off;
+
+CREATE TABLE app_user_new (
+    id INTEGER NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    access_level TEXT CHECK(access_level IN ('admin', 'editor', 'viewer')) DEFAULT 'editor',
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO app_user_new (id, username, password_hash, access_level)
+  SELECT id, username, password_hash, access_level
+  FROM app_user;
+
+DROP TABLE app_user;
+
+ALTER TABLE app_user_new RENAME TO app_user;
+
+CREATE INDEX user_username_idx ON app_user(username);
 
 CREATE TRIGGER on_app_user_update
     AFTER UPDATE ON app_user
 BEGIN
     UPDATE app_user SET modified_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
 END;
+
+PRAGMA foreign_keys=on;
