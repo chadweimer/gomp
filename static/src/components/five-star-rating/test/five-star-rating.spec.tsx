adc2523b6a31039639eecb1e5da075b63e5de48b
Chad Weimer <git@cwmr.dev>
2023-08-14 08:50:39 -0700

    Added more tests

--- static/src/components/five-star-rating/test/five-star-rating.spec.tsx
+++ static/src/components/five-star-rating/test/five-star-rating.spec.tsx
@@ -1,7 +1,46 @@
+import { newSpecPage } from '@stencil/core/testing';
 import { FiveStarRating } from '../five-star-rating';
 
 describe('five-star-rating', () => {
   it('builds', () => {
     expect(new FiveStarRating()).toBeTruthy();
+  });
+
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [FiveStarRating],
+      html: '<five-star-rating></five-star-rating>',
+    });
+    expect(page.root.shadowRoot).toEqualHtml(`
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+    `);
+  });
+
+  it('renders value', async () => {
+    const page = await newSpecPage({
+      components: [FiveStarRating],
+      html: '<five-star-rating value="3.5"></five-star-rating>',
+    });
+    expect(page.root.shadowRoot).toEqualHtml(`
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="icon whole selected" icon="star" size="small"></ion-icon>
+      <ion-icon class="half icon selected" icon="star" size="small"></ion-icon>
+    `);
   });
 });
