7a684b89f684035376355f743bc2920a77335588
Chad Weimer <git@cwmr.dev>
2023-08-14 20:51:15 +0000

    NoteEditor tests

--- static/src/components/note-editor/test/note-editor.spec.tsx
+++ static/src/components/note-editor/test/note-editor.spec.tsx
@@ -1,7 +1,30 @@
+import { h } from '@stencil/core';
+import { newSpecPage } from '@stencil/core/testing';
 import { NoteEditor } from '../note-editor';
+import { Note } from '../../../generated';
 
 describe('note-editor', () => {
   it('builds', () => {
     expect(new NoteEditor()).toBeTruthy();
+  });
+
+  it('no initial value', async () => {
+    const page = await newSpecPage({
+      components: [NoteEditor],
+      html: '<note-editor></note-editor>',
+    });
+    const textArea = page.root.querySelector('ion-textarea');
+    expect(textArea).toBeTruthy();
+    expect(textArea).toEqualAttribute('value', '');
+  });
+
+  it('bind to note', async () => {
+    const note: Note = { text: 'Some text' };
+    const page = await newSpecPage({
+      components: [NoteEditor],
+      template: () => (<note-editor note={note}></note-editor>),
+    });
+    const textArea = page.root.querySelector('ion-textarea');
+    expect(textArea).toEqualAttribute('value', note.text);
   });
 });
