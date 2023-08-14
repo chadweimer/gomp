a0af645b7fb06bfc078b0c43c3f08d03d7536301
Chad Weimer <git@cwmr.dev>
2023-08-14 21:41:27 +0000

    SortBySelector tests

--- static/src/components/sort-by-selector/test/sort-by-selector.spec.tsx
+++ static/src/components/sort-by-selector/test/sort-by-selector.spec.tsx
@@ -1,7 +1,36 @@
+import { h } from '@stencil/core';
+import { newSpecPage } from '@stencil/core/testing';
 import { SortBySelector } from '../sort-by-selector';
+import { SortBy } from '../../../generated';
 
 describe('sort-by-selector', () => {
   it('builds', () => {
     expect(new SortBySelector()).toBeTruthy();
   });
+
+  it('defaults to Name', async () => {
+    const page = await newSpecPage({
+      components: [SortBySelector],
+      html: '<sort-by-selector></sort-by-selector>',
+    });
+    const component = page.rootInstance as SortBySelector;
+    expect(component.sortBy).toEqual(SortBy.Name);
+    const radioGroup = page.root.shadowRoot.querySelector('ion-radio-group');
+    expect(radioGroup).toBeTruthy();
+    expect(radioGroup).toEqualAttribute('value', SortBy.Name);
+  });
+
+  for (const value in SortBy)
+  {
+    it('can be set to ' + value, async () => {
+      const page = await newSpecPage({
+        components: [SortBySelector],
+        template: () => (<sort-by-selector sortBy={value as SortBy}></sort-by-selector>),
+      });
+      const component = page.rootInstance as SortBySelector;
+      expect(component.sortBy).toEqual(value);
+      const radioGroup = page.root.shadowRoot.querySelector('ion-radio-group');
+      expect(radioGroup).toEqualAttribute('value', value);
+    });
+  }
 });
