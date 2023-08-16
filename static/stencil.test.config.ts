a5fdc7bc41bc643d754d88e69f6bb8081a3e57a3
Chad Weimer <git@cwmr.dev>
2023-08-15 22:14:51 -0700

    Allow e2e tests to actually work

---
+++ static/stencil.test.config.ts
@@ -0,0 +1,20 @@
+import { Config } from '@stencil/core';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.css',
+  taskQueue: 'async',
+  outputTargets: [{
+    type: 'www',
+    baseUrl: '/',
+    serviceWorker: null,
+    copy: [
+      { src: 'default-home-image.png' }
+    ]
+  }],
+  testing: {
+    browserHeadless: 'new',
+  },
+};
