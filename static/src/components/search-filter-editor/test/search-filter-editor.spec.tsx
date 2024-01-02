f218e2e7d0d2ee75f907684f877295956fe7135d
Chad Weimer <git@cwmr.dev>
2024-01-02 13:22:40 -0800

    Just a small test

--- static/src/components/search-filter-editor/test/search-filter-editor.spec.tsx
+++ static/src/components/search-filter-editor/test/search-filter-editor.spec.tsx
@@ -10,3 +10,15 @@ describe('search-filter-editor', () => {
     expect(page.rootInstance).toBeInstanceOf(SearchFilterEditor);
   });
 });
+
+describe('shows saved filter loader', () => {
+  it('builds', async () => {
+    const page = await newSpecPage({
+      components: [SearchFilterEditor],
+      html: '<search-filter-editor show-saved-loader></search-filter-editor>',
+    });
+    expect(page.rootInstance).toBeInstanceOf(SearchFilterEditor);
+    const component = page.rootInstance as SearchFilterEditor;
+    expect(component.showSavedLoader).toBeTruthy();
+  });
+});
