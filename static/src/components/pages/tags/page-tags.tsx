c6843a200b75a3c695e641ad6eb5e269d7cbdb7f
Chad Weimer <git@cwmr.dev>
2025-06-09 22:32:59 -0700

    Stubbed tags page

---
+++ static/src/components/pages/tags/page-tags.tsx
@@ -0,0 +1,52 @@
+import { Component, h, Host, Method, State } from '@stencil/core';
+import { recipesApi } from '../../../helpers/api';
+import { isNull, redirect } from '../../../helpers/utils';
+import state from '../../../stores/state';
+import { getDefaultSearchFilter } from '../../../models';
+
+@Component({
+  tag: 'page-tags',
+  styleUrl: 'page-tags.css'
+})
+export class PageTags {
+  @State() tags: { [tag: string]: number } | null;
+
+  async connectedCallback() {
+    await this.load();
+  }
+
+  @Method()
+  async activatedCallback() {
+    await this.load();
+  }
+
+  render() {
+    return (
+      <Host>
+        {!isNull(this.tags) ?
+          Object.entries(this.tags).map(([key, val]) =>
+        	<div>{key}</div>
+          )
+          : ''}
+      </Host>
+    );
+  }
+
+  private async load() {
+    try {
+      this.tags = await recipesApi.getAllTags();
+    } catch (ex) {
+      this.tags = null;
+      console.error(ex);
+    }
+  }
+
+  private async onTagClicked(tag: string) {
+    const filter = getDefaultSearchFilter();
+    state.searchFilter = {
+      ...filter,
+      tags: [tag]
+    };
+    await redirect('/recipes');
+  }
+}
