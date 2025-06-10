a99b9aab4b22c939bf89e84b74747fc8704b85f2
Chad Weimer <1165476+chadweimer@users.noreply.github.com>
2025-06-10 09:04:15 -0700

    Add sorting

--- static/src/components/pages/tags/page-tags.tsx
+++ static/src/components/pages/tags/page-tags.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Host, Method, State } from '@stencil/core';
+import { SortDir } from '../../../generated';
 import { recipesApi } from '../../../helpers/api';
 import { isNull, redirect } from '../../../helpers/utils';
 import state from '../../../stores/state';
@@ -10,6 +11,7 @@ import { getDefaultSearchFilter } from '../../../models';
 })
 export class PageTags {
   @State() tags: { [tag: string]: number } | null;
+  @State() sortDir: SortDir = SortDir.Desc;
 
   async connectedCallback() {
     await this.load();
@@ -23,13 +25,24 @@ export class PageTags {
   render() {
     return (
       <Host>
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons class="ion-justify-content-center">
+              <ion-button color="secondary" onClick={() => this.sortDir === SortDir.Asc ? SortDir.Desc : SortDir.Asc}>
+                <ion-icon slot="icon-only" icon={this.sortDir === SortDir.Asc ? 'arrow-up' : 'arrow-down'} />
+              </ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+
         <ion-content>
           <ion-grid class="no-pad">
             <ion-row>
               {!isNull(this.tags) ?
-                Object.entries(this.tags).map(([key, val]) =>
+                Object.entries(this.tags).toSorted(([keyA, valA], [keyB, valB]) => this.compare(valA, valB)).map(([key, val]) =>
                   <ion-col key={key} size="12" size-md="6" size-lg="4" size-xl="3">
                     <ion-item href="/recipes" onClick={() => this.onTagClicked(key)}>
+                      <ion-icon slot="start" name="bookmark" />
                       <ion-label>{key}</ion-label>
                       <ion-note slot="end">{val}</ion-note>
                     </ion-item>
@@ -59,5 +72,14 @@ export class PageTags {
       tags: [tag]
     };
     // await redirect('/recipes');
+  }
+
+  private compare(a: number, b: number) {
+    if( a < b ){
+      return this.sortDir === SortDir.Asc ? -1 : 1;
+    } else if( a > b ){
+      return this.sortDir === SortDir.Asc ? 1 : -1;
+    } 
+    return 0;
   }
 }
