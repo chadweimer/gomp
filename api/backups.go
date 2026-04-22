40a1f5db22441a99c537b414f4d3d2eb33f87c5e
Chad Weimer <git@cwmr.dev>
2026-04-22 11:41:35 -0700

    Add some debug code

--- api/backups.go
+++ api/backups.go
@@ -10,34 +10,42 @@ import (
 	"time"
 
 	"github.com/chadweimer/gomp/fileaccess"
+	"github.com/chadweimer/gomp/middleware"
 )
 
 func (h apiHandler) CreateBackup(ctx context.Context, _ CreateBackupRequestObject) (CreateBackupResponseObject, error) {
+	logger := middleware.GetLoggerFromContext(ctx)
+
 	// Export all data from the database
 	exportedData, err := h.db.Backups().Export(ctx)
 	if err != nil {
+		logger.ErrorContext(ctx, "Failed to export backup data", "error", err)
 		return nil, err
 	}
 
-	if err = os.MkdirAll("tmp", 0750); err != nil {
-		return nil, err
-	}
-	tempFile, err := os.CreateTemp("tmp", "backup-*.zip")
+	tempFile, err := os.CreateTemp("", "backup-*.zip")
 	if err != nil {
+		logger.ErrorContext(ctx, "Failed to create temp file", "error", err)
 		return nil, err
 	}
 	defer tempFile.Close()
 	defer os.Remove(tempFile.Name())
 
+	logger.DebugContext(ctx, "Created temp file")
+
 	err = fileaccess.CreateZip(tempFile, func(writer *zip.Writer) error {
 		// // Write the backup to JSON
 		buf, err := json.MarshalIndent(exportedData, "", "  ")
 		if err != nil {
+			logger.ErrorContext(ctx, "Failed to marshal backup data", "error", err)
 			return err
 		}
 		if err := fileaccess.WriteFileToZip("data.json", bytes.NewBuffer(buf), writer); err != nil {
+			logger.ErrorContext(ctx, "Failed to write backup data to zip", "error", err)
 			return err
 		}
+
+		logger.DebugContext(ctx, "Copied backup data to zip")
 
 		// Copy all uploads to the backup directory
 		return fileaccess.CopyDirectoryToZip(h.fs, fileaccess.RootUploadPath, writer)
