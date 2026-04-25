002741fba8461bb933ce47275dcdc81677822f7b
Chad Weimer <git@cwmr.dev>
2026-04-25 15:48:17 -0700

    Drop recipe_image table

---
+++ db/migrations/postgres/0025_remove_recipe_images.down.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+ALTER TABLE recipe
+ADD COLUMN image_id INTEGER REFERENCES recipe_image(id) ON DELETE SET NULL;
+
+ALTER TABLE recipe
+DROP COLUMN main_image_name;
+
+CREATE TABLE recipe_image (
+    id SERIAL NOT NULL PRIMARY KEY,
+    recipe_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT NOT NULL,
+    thumbnail_url TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(recipe_id) REFERENCES recipe(id) ON DELETE CASCADE
+);
+CREATE INDEX recipe_image_recipe_id_idx ON recipe_image(recipe_id);
+
+COMMIT;
