8fbdc59a35c56aa1b20177345c83b519b8e9e218
Chad Weimer <git@cwmr.dev>
2026-04-27 16:44:53 -0700

    Fix migration

--- db/migrations/postgres/0025_remove_recipe_images.up.sql
+++ db/migrations/postgres/0025_remove_recipe_images.up.sql
@@ -4,9 +4,9 @@ ALTER TABLE recipe
 ADD COLUMN main_image_name TEXT NOT NULL DEFAULT '';
 
 UPDATE recipe
-SET main_image_name = (
-  SELECT COALESCE(recipe_image.name, '') FROM recipe_image WHERE recipe_image.id = recipe.image_id
-);
+SET main_image_name = COALESCE((
+  SELECT recipe_image.name FROM recipe_image WHERE recipe_image.id = recipe.image_id
+), '');
 
 ALTER TABLE recipe
 DROP COLUMN image_id;
