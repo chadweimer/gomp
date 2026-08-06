56ea66a2ce2d2a1b1ba345f66b7d5145c7463fbd
Chad Weimer <git@cwmr.dev>
2026-08-06 10:05:38 -0700

    Leverage encoding.TextUnmarshaler interface for proxies configuration

--- config_test.go
+++ config_test.go
@@ -1,20 +1,21 @@
 package main
 
-import "testing"
+import (
+	"net"
+	"testing"
+)
 
 func TestConfig_validate(t *testing.T) {
 	type fields struct {
 		Port           int
 		BaseAssetsPath string
 		SecureKeys     []string
-		TrustedProxies []string
 	}
 	init := func(opts ...func(f *fields)) fields {
 		f := fields{
 			Port:           1234,
 			BaseAssetsPath: "/path/to/assets",
 			SecureKeys:     []string{"secure key"},
-			TrustedProxies: []string{"192.168.0.0/16", "127.0.0.1"},
 		}
 		for _, opt := range opts {
 			opt(&f)
@@ -52,13 +53,6 @@ func TestConfig_validate(t *testing.T) {
 			}),
 			wantErr: true,
 		},
-		{
-			name: "Invalid Trusted Proxies",
-			fields: init(func(f *fields) {
-				f.TrustedProxies = []string{"invalid"}
-			}),
-			wantErr: true,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -66,10 +60,61 @@ func TestConfig_validate(t *testing.T) {
 				Port:           tt.fields.Port,
 				BaseAssetsPath: tt.fields.BaseAssetsPath,
 				SecureKeys:     tt.fields.SecureKeys,
-				TrustedProxies: tt.fields.TrustedProxies,
 			}
 			if got := c.validate(); tt.wantErr != (got != nil) {
 				t.Errorf("Config.validate() = %v, want error? %v", got, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestTrustedProxy_UnmarshalText(t *testing.T) {
+	tests := []struct {
+		name    string
+		text    string
+		want    net.IPNet
+		wantErr bool
+	}{
+		{
+			name: "Valid CIDR",
+			text: "192.168.0.0/16",
+			want: net.IPNet{
+				IP:   net.ParseIP("192.168.0.0"),
+				Mask: net.CIDRMask(16, 32),
+			},
+			wantErr: false,
+		},
+		{
+			name: "Valid IP",
+			text: "127.0.0.1",
+			want: net.IPNet{
+				IP:   net.ParseIP("127.0.0.1"),
+				Mask: net.CIDRMask(32, 32),
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Invalid IP",
+			text:    "invalid",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var tp TrustedProxy
+			gotErr := tp.UnmarshalText([]byte(tt.text))
+			got := tp.IPNet
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("UnmarshalText() failed: %v", gotErr)
+				}
+				return
+			}
+			if got.String() != tt.want.String() {
+				t.Errorf("UnmarshalText() = %v, want %v", got.String(), tt.want.String())
+			}
+			if tt.wantErr {
+				t.Fatal("UnmarshalText() succeeded unexpectedly")
 			}
 		})
 	}
