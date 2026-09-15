fab7a9f30eeb81e34d7bdb9527612d022842f86d
Chad Weimer <git@cwmr.dev>
2026-09-14 17:14:16 -0700

    Added support for secret files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
-
 set -e
+
+load_secret_file() {
+  local var_name="$1"
+  local file_path="$2"
+
+  if [[ -f "$file_path" ]]; then
+    export "${var_name}"="$(cat "$file_path" | tr -d '\n' | tr -d '\r')"
+  fi
+}
+
+# Load any secret files specified
+if [[ -n "$DATABASE_URL_FILE" ]]; then
+  load_secret_file "DATABASE_URL" "$DATABASE_URL_FILE"
+fi
+if [[ -n "$SECURE_KEY_FILE" ]]; then
+  load_secret_file "SECURE_KEY" "$SECURE_KEY_FILE"
+fi
 
 # First migrate the DB
 ./gomp db migrate up
 
-# Then run the application
+# Then serve the application
 ./gomp serve
