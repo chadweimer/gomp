837b75cdcce7175451285f7f6d0c601375954b65
Chad Weimer <1165476+chadweimer@users.noreply.github.com>
2026-09-18 08:20:24 -0700

    Adjusted docker entrypoint to support migration on custom command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,23 +2,23 @@
 set -e
 
 load_secret_file() {
-  if [ -f "$2" ]; then
-    export "$1=$(tr -d '\n\r' < "$2")"
+  var_name="$1"
+  file_path="$2"
+
+  if [ -f "$file_path" ]; then
+    export "$var_name=$(tr -d '\n\r' < "$file_path")"
   else
-    echo "ERROR: file '$2' specified in ${1}_FILE does not exist" >&2
+    echo "ERROR: file '$file_path' specified in ${var_name}_FILE does not exist" >&2
     exit 1
   fi
 }
 
 # Load any secret files specified
 [ -n "$DATABASE_URL_FILE" ] && load_secret_file "DATABASE_URL" "$DATABASE_URL_FILE"
-[ -n "$SECURE_KEY_FILE" ] && load_secret_file "SECURE_KEY" "$SECURE_KEY_FILE"
+[ -n "$SECURE_KEY_FILE"   ] && load_secret_file "SECURE_KEY"   "$SECURE_KEY_FILE"
 
-if [ $# -gt 0 ]; then
-  exec ./gomp "$@"
-else
-  # First migrate the DB
+if [ "${DATABASE_SKIP_MIGRATION:-false}" != "true" ]; then
   ./gomp db migrate up
-  # Then serve the application
-  exec ./gomp serve
 fi
+
+exec ./gomp "$@"
