53f4b93b512f332d330e44d09faf445a7170f6a9
Chad Weimer <1165476+chadweimer@users.noreply.github.com>
2026-09-17 21:53:20 -0700

    Update docker-entrypoint.sh to support specifying CMD

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,23 @@
 set -e
 
 load_secret_file() {
-  local var_name="$1"
-  local file_path="$2"
-
-  if [[ -f "$file_path" ]]; then
-    export "${var_name}"="$(cat "$file_path" | tr -d '\n' | tr -d '\r')"
+  if [ -f "$2" ]; then
+    export "$1=$(tr -d '\n\r' < "$2")"
+  else
+    echo "ERROR: file '$2' specified in ${1}_FILE does not exist" >&2
+    exit 1
   fi
 }
 
 # Load any secret files specified
-if [[ -n "$DATABASE_URL_FILE" ]]; then
-  load_secret_file "DATABASE_URL" "$DATABASE_URL_FILE"
-fi
-if [[ -n "$SECURE_KEY_FILE" ]]; then
-  load_secret_file "SECURE_KEY" "$SECURE_KEY_FILE"
-fi
+[ -n "$DATABASE_URL_FILE" ] && load_secret_file "DATABASE_URL" "$DATABASE_URL_FILE"
+[ -n "$SECURE_KEY_FILE" ] && load_secret_file "SECURE_KEY" "$SECURE_KEY_FILE"
 
-# First migrate the DB
-./gomp db migrate up
-
-# Then serve the application
-./gomp serve
+if [ $# -gt 0 ]; then
+  exec ./gomp "$@"
+else
+  # First migrate the DB
+  ./gomp db migrate up
+  # Then serve the application
+  exec ./gomp serve
+fi
